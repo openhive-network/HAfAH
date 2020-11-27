825a4543b6390d7666f0175c0e902df160ea49de
Dariusz Kedzierski <dkedzierski@syncad.com>
2020-11-27 10:48:16 +0100

    Use logger instead print

--- hivemind/tavern/validate_response.py
+++ hivemind/tavern/validate_response.py
@@ -42,7 +42,7 @@ def remove_tag(data, tags_to_remove):
 def compare_response_with_pattern(response, method=None, directory=None, ignore_tags=None, error_response=False):
   """ This method will compare response with pattern file """
   t_start = perf()
-  print("compare_response_with_pattern for {} started at: {}".format(directory + "/" + method, datetime.now()))
+  log.warning("compare_response_with_pattern for {} started at: {}".format(directory + "/" + method, datetime.now()))
 
   response_fname = directory + "/" + method + RESPONSE_FILE_EXT
   if os.path.exists(response_fname):
@@ -84,5 +84,5 @@ def compare_response_with_pattern(response, method=None, directory=None, ignore_
     save_json(response_fname, result)
     msg = "Differences detected between response and pattern."
     raise PatternDiffException(msg)
-  print("compare_response_with_pattern for {} ended at: {}".format(directory + "/" + method, datetime.now()))
-  print("comparing {} took {:4f}s".format(directory + "/" + method, perf() - t_start))
+  log.warning("compare_response_with_pattern for {} ended at: {}".format(directory + "/" + method, datetime.now()))
+  log.warning("comparing {} took {:4f}s".format(directory + "/" + method, perf() - t_start))
