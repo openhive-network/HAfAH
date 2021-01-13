ccd2b2e7937e62187002a1e2f97009c8c031b239
Dariusz Kędzierski <dkedzierski@syncad.com>
2021-01-13 15:34:38 +0100

    Unified validate_response file for tavern and tavern_full_sync tests

--- scripts/validate_response.py
+++ scripts/validate_response.py
@@ -5,6 +5,9 @@ from time import perf_counter as perf
 class PatternDiffException(Exception):
   pass
 
+class NoResultException(Exception):
+  pass
+
 def json_pretty_string(json_obj):
   from json import dumps
   return dumps(json_obj, sort_keys=True, indent=2)
@@ -96,3 +99,34 @@ def compare_response_with_pattern(response, method=None, directory=None, ignore_
     save_json(response_fname, result)
     msg = "Differences detected between response and pattern."
     raise PatternDiffException(msg)
+
+def has_valid_response(response, method=None, directory=None, error_response=False, response_fname=None):
+  import os
+  if not response_fname:
+    response_fname = directory + "/" + method + RESPONSE_FILE_EXT
+
+  if os.path.exists(response_fname):
+    os.remove(response_fname)
+
+  response_json = response.json()
+  if error_response:
+    correct_response = response_json.get("error", None)
+  else:
+    correct_response = response_json.get("result", None)
+
+  # disable coparison with pattern on demand
+  # and save 
+  if bool(os.getenv('TAVERN_DISABLE_COMPARATOR', False)):
+    test_id = response_json.get("id", None)
+    print(test_id)
+    if test_id is not None:
+      with open("benchmark.csv", 'a') as benchmark_file:
+        writer = csv.writer(benchmark_file)
+        writer.writerow([directory + "/" + method, perf() - get_time(test_id), int(response.headers.get("Content-Length", 0))])
+    return
+
+  save_json(response_fname, response_json)
+  if correct_response is None:
+    msg = "Error detected in response: result is null, json object was expected"
+    raise NoResultException(msg)
+
