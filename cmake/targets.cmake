19b516107395f7591ca475c6bf49f18be33eebf5
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-03-08 07:32:44 +0100

    compiler options per target

--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -2,7 +2,7 @@ MACRO( ADD_RUNTIME_LOADED_LIB target_name sources )
     MESSAGE( INFO ${sources})
     ADD_LIBRARY( ${target_name} SHARED ${sources} )
 
-    TARGET_INCLUDE_DIRECTORIES( ${target_name} PRIVATE "." )
+    SETUP_COMPILER( ${target_name} )
 
     ADD_BOOST_LIBRARIES( ${target_name} )
     ADD_POSTGRES_LIBRARIES( ${target_name} )
