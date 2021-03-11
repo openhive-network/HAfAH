51d5d3ab35012a70a802be61ae7d4f26b822610f
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-03-11 14:19:21 +0100

    postgres log support __FILE_ and _LINE_ with psql configuration option log_error_verbosity

--- fork_extension/include/logger.hpp
+++ fork_extension/include/logger.hpp
@@ -2,19 +2,16 @@
 
 #include "include/postgres_includes.hpp"
 
-#define STRINGIZE(x) STRINGIZE2(x)
-#define STRINGIZE2(x) #x
+#define LOG_TO_POSTGRES( _level, _message, ... )      \
+  elog( _level, _message, ##__VA_ARGS__ )             \
 
-#define LOG_TO_POSTGRES( _level, _file, _line, _message, ... )                        \
-  elog( _level, _file ":" _line ": " _message, ##__VA_ARGS__ )                        \
+#define LOG_WARNING( _message, ... )                  \
+  LOG_TO_POSTGRES( WARNING, _message, ##__VA_ARGS__ ) \
 
-#define LOG_WARNING( _message, ... )                                                  \
-  LOG_TO_POSTGRES( WARNING, __FILE__, STRINGIZE(__LINE__), _message, ##__VA_ARGS__ )  \
+#define LOG_INFO( _message, ... )                     \
+  LOG_TO_POSTGRES( INFO, _message, ##__VA_ARGS__ )    \
 
-#define LOG_INFO( _message, ... )                                                     \
-  LOG_TO_POSTGRES( INFO, __FILE__, STRINGIZE(__LINE__), _message, ##__VA_ARGS__ )     \
-
-
-#define LOG_ERROR( _message, ... )                                                    \
-  LOG_TO_POSTGRES( ERROR, __FILE__, STRINGIZE(__LINE__), _message, ##__VA_ARGS__ )    \
+// WARNING! log error will finish process because Postgres uses there __builtin_unreachable()
+#define LOG_ERROR( _message, ... )                     \
+  LOG_TO_POSTGRES( ERROR, _message, ##__VA_ARGS__ )    \
 
