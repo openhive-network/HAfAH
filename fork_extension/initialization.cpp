e811175f0e02330e5e4d3c647a19b52f90be7efc
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-03-11 12:34:47 +0100

    workaraund for recursive _PG_init calls

--- fork_extension/initialization.cpp
+++ fork_extension/initialization.cpp
@@ -2,10 +2,26 @@
 
 #include "include/initializer.hpp"
 
+namespace {
+    bool is_initialized = false;
+}
+
+
 extern "C" {
   PG_MODULE_MAGIC;
 
 void _PG_init(void) {
+  /* BEGIN WORKAROUND
+   * The problem is that during initialization we want to add functions from the extension plugin (this *.so), what leads to
+   * recursive calls of _PG_init because postgres mark the dll as loaded (by adds it to the list of loaded plugins) after
+   * finishing _PG_init. Look at dfmgr.c in postgres sources. The side effect of this workaround  is additional one more
+   * reduntand *.so file in postgres internal lists.
+   */
+  if ( is_initialized ) {
+    return;
+  }
+  is_initialized = true;
+  // END WORKAROUND
   ForkExtension::Initializer initializer;
 }
 
