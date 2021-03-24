8630a2aa185b78a9a08b6ca5aa19bf05c54cb465
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-03-24 08:50:19 +0100

    improve test for update

--- fork_extension/sql/example_update.sql
+++ fork_extension/sql/example_update.sql
@@ -16,29 +16,33 @@ CREATE TYPE custom_type AS (
 DROP TABLE IF EXISTS src_table CASCADE;
 CREATE TABLE src_table(id  SERIAL PRIMARY KEY, smth INTEGER, name TEXT, values FLOAT[], data custom_type, name2 VARCHAR, num NUMERIC(3,2) );
 
---2. Create trigger ( function on_table_change()  was added by the plugin during loading )
-CREATE TRIGGER on_src_table_change AFTER UPDATE ON src_table
-    REFERENCING NEW TABLE AS new_table OLD TABLE AS old_table
-    FOR EACH STATEMENT EXECUTE PROCEDURE on_table_change();
+DROP TABLE IF EXISTS src_table_pattern CASCADE;
+CREATE TABLE src_table_pattern(id  SERIAL PRIMARY KEY, smth INTEGER, name TEXT, values FLOAT[], data custom_type, name2 VARCHAR, num NUMERIC(3,2) );
 
---3. Make operations on src_table
---3.a Insert 10000 rows to src table, each of them will be copied to the tuples table
 INSERT INTO src_table ( smth, name, values, data, name2, num ) 
 SELECT gen.id, val.name, val.arr, val.rec, val.name2, val.num
 FROM generate_series(1, 10000) AS gen(id)
 JOIN ( VALUES( 'temp1', '{{0.25, 3.4, 6}}'::FLOAT[], ROW(1, 5.8, '123abc')::custom_type, 'padu'::VARCHAR, 2.123::NUMERIC(3,2) ) ) as val(name,arr,rec, name2, num) ON True;
 
---3.b update rows
+INSERT INTO src_table_pattern SELECT * FROM src_table;
+
+--2. Create trigger ( function on_table_change()  was added by the plugin during loading )
+CREATE TRIGGER on_src_table_change AFTER UPDATE ON src_table
+    REFERENCING NEW TABLE AS new_table OLD TABLE AS old_table
+    FOR EACH STATEMENT EXECUTE PROCEDURE on_table_change();
+
+--3. Make operations on src_table update rows
 UPDATE src_table SET name = 'changed name';
 
 --4 deserialize saved tuples to rows in src_table
-SELECT back_from_fork(); -- 51ms,41ms,41ms
+SELECT back_from_fork();
 
---4.a check that tuples name='tmp1'
-SELECT * FROM src_table  LIMIT 100;
+--5 check that back from fork works
+SELECT * FROM src_table_pattern EXCEPT SELECT * FROM src_table;
+
 
 -- Cleanup things added by plugin
-DROP FUNCTION IF EXISTS on_table_change CASCADE;
-DROP FUNCTION IF EXISTS back_from_fork CASCADE;
-DROP TABLE IF EXISTS tuples;
+-- DROP FUNCTION IF EXISTS on_table_change CASCADE;
+-- DROP FUNCTION IF EXISTS back_from_fork CASCADE;
+-- DROP TABLE IF EXISTS tuples;
 
