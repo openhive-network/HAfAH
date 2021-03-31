1fd4de391b968c7d3d1b14859560b909d405f97c
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-03-31 08:15:21 +0200

    add test for SPiSession singleton

--- tests/unit/psql_utils/spi_session_test.cpp
+++ tests/unit/psql_utils/spi_session_test.cpp
@@ -29,6 +29,25 @@ BOOST_AUTO_TEST_CASE( positivie_session_create_and_destroy )
   BOOST_CHECK_NO_THROW( PsqlTools::PsqlUtils::Spi::SpiSession::create() );
 }
 
+BOOST_AUTO_TEST_CASE( positivie_singleton_check )
+{
+  auto spi_mock = SpiMock::create_and_get();
+  {
+    InSequence seq;
+    EXPECT_CALL(*spi_mock, SPI_connect())
+      .Times(1)
+      .WillOnce(Return(SPI_OK_CONNECT));
+
+    EXPECT_CALL(*spi_mock, SPI_finish())
+      .Times(1)
+      .WillOnce(Return(SPI_OK_CONNECT));
+  }
+
+  auto session1 = PsqlTools::PsqlUtils::Spi::SpiSession::create();
+  auto session2 = PsqlTools::PsqlUtils::Spi::SpiSession::create();
+  BOOST_CHECK_EQUAL( session1, session2 );
+}
+
 BOOST_AUTO_TEST_CASE( negative_session_create )
 {
   auto spi_mock = SpiMock::create_and_get();
