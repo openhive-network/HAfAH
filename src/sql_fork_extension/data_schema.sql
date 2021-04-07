72b9116e23f82e801fda5a25fe1d4956b138967f
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-04-07 13:08:37 +0200

    more sql improvments

--- src/sql_fork_extension/data_schema.sql
+++ src/sql_fork_extension/data_schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE hive_contexts(
+CREATE TABLE IF NOT EXISTS hive_contexts(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     current_block_num INTEGER NOT NULL,
     CONSTRAINT uq_hive_context_name UNIQUE ( name )
 );
 
-CREATE TABLE hive_registered_tables(
+CREATE TABLE IF NOT EXISTS hive_registered_tables(
    id SERIAL PRIMARY KEY,
    context_id INTEGER NOT NULL,
    origin_table_name TEXT NOT NULL,
@@ -13,11 +13,12 @@ CREATE TABLE hive_registered_tables(
    CONSTRAINT fk_hive_registered_tables_context FOREIGN KEY(context_id) REFERENCES hive_contexts( id )
 );
 
-CREATE TABLE hive_triggers_operations(
+CREATE TABLE IF NOT EXISTS hive_triggers_operations(
    id SERIAL PRIMARY KEY,
    name TEXT NOT NULL,
    CONSTRAINT uq_hive_triggers_operations_name UNIQUE( name )
 );
 
 INSERT INTO hive_triggers_operations( id, name )
-VALUES ( 0, 'INSERT' ), ( 1, 'DELETE' ), (2, 'UPDATE' );
+VALUES ( 0, 'INSERT' ), ( 1, 'DELETE' ), (2, 'UPDATE' )
+ON CONFLICT DO NOTHING;
