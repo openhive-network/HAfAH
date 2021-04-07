dc65da65982fccfd59662f67c63a95fc554404c8
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-04-07 12:52:38 +0200

    sql: context next block

--- src/sql_fork_extension/context.sql
+++ src/sql_fork_extension/context.sql
@@ -10,3 +10,19 @@ BEGIN
 END;
 $BODY$
 ;
+
+DROP FUNCTION IF EXISTS hive_context_next_block;
+CREATE FUNCTION hive_context_next_block( _name TEXT )
+    RETURNS INTEGER
+    LANGUAGE 'sql'
+    VOLATILE
+AS
+$BODY$
+    UPDATE hive_contexts
+    SET current_block_num = CASE WHEN current_block_num IS NULL THEN 0 ELSE  current_block_num + 1 END
+    WHERE name = _name
+    RETURNING current_block_num
+$BODY$
+;
+
+--TODO: remove context ?
---
+++ tests/integration/functional/sql_fork_extension/context_next_block_test.sql
@@ -0,0 +1,52 @@
+DROP FUNCTION IF EXISTS test_given;
+CREATE FUNCTION test_given()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+    PERFORM hive_create_context( 'my_context' );
+    PERFORM hive_create_context( 'my_context2' );
+END;
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_when;
+CREATE FUNCTION test_when()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+DECLARE
+    __block1 INTEGER := -1;
+    __block2 INTEGER := -1;
+BEGIN
+    SELECT  hive_context_next_block( 'my_context' ) INTO __block1;
+    PERFORM hive_context_next_block( 'my_context2' );
+    SELECT hive_context_next_block( 'my_context2' ) INTO __block2;
+
+    ASSERT __block1 = 0;
+    ASSERT __block2 = 1;
+END
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_then;
+CREATE FUNCTION test_then()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+STABLE
+AS
+$BODY$
+BEGIN
+    ASSERT EXISTS ( SELECT FROM hive_contexts WHERE name = 'my_context' AND current_block_num = 0 );
+    ASSERT EXISTS ( SELECT FROM hive_contexts WHERE name = 'my_context2' AND current_block_num = 1 );
+END
+$BODY$
+;
+
+SELECT test_given();
+SELECT test_when();
+SELECT test_then();
