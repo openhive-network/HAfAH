b53d4ce940ce15ea23f10bad68d2adac9c70c9ef
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-05-17 14:22:52 +0200

    app_create_context sets irreversible block from hive.blocks

--- src/hive_fork/app_api.sql
+++ src/hive_fork/app_api.sql
@@ -5,13 +5,19 @@ CREATE OR REPLACE FUNCTION hive.app_create_context( _name TEXT )
 AS
 $BODY$
 BEGIN
+    -- Any context always starts with block before genesis, the app may detach the context and execute 'massive sync'
+    -- after massive sync the application must attach its context to last already synced block
     INSERT INTO hive.app_context(
            name
          , current_block_num
          , irreversible_block
          , is_attached
          , events_id)
-     VALUES( _name, -1, -1, TRUE, NULL );
+    SELECT cdata.name, cdata.block_num, COALESCE( hb.num, 0 ), cdata.is_attached, cdata.events_id
+    FROM
+        ( VALUES ( _name, 0, TRUE, NULL::BIGINT ) ) as cdata( name, block_num, is_attached, events_id )
+        LEFT JOIN ( SELECT hb.num FROM hive.blocks hb ORDER BY hb.num DESC LIMIT 1 ) as hb ON TRUE
+    ;
 END;
 $BODY$
 ;
--- tests/integration/functional/hive_fork/app_api/create_context_test.sql
+++ tests/integration/functional/hive_fork/app_api/create_context_test.sql
@@ -20,6 +20,9 @@ AS
 $BODY$
 BEGIN
     PERFORM hive.app_create_context( 'context' );
+
+    -- check if correct irreversibe block is set
+    INSERT INTO hive.blocks VALUES( 101, '\xBADD', '\xCAFE', '2016-06-22 19:10:25-07'::timestamp );
     PERFORM hive.app_create_context( 'context2');
 END
 $BODY$
@@ -33,11 +36,11 @@ STABLE
 AS
 $BODY$
 BEGIN
-    ASSERT EXISTS ( SELECT FROM hive.app_context WHERE name = 'context' AND current_block_num = -1 AND irreversible_block = -1 AND events_id IS NULL AND is_attached = TRUE );
-    ASSERT EXISTS ( SELECT FROM hive.app_context WHERE name = 'context2' AND current_block_num = -1 AND irreversible_block = -1  AND events_id IS NULL AND is_attached = TRUE );
+    ASSERT EXISTS ( SELECT FROM hive.app_context WHERE name = 'context' AND current_block_num = 0 AND irreversible_block = 0 AND events_id IS NULL AND is_attached = TRUE );
+    ASSERT EXISTS ( SELECT FROM hive.app_context WHERE name = 'context2' AND current_block_num = 0 AND irreversible_block = 101  AND events_id IS NULL AND is_attached = TRUE );
 
-    ASSERT EXISTS ( SELECT FROM hive.context WHERE name = 'context' AND current_block_num = -1 AND irreversible_block = -1 AND is_attached = TRUE );
-    ASSERT EXISTS ( SELECT FROM hive.context WHERE name = 'context2' AND current_block_num = -1 AND irreversible_block = -1 AND is_attached = TRUE );
+    ASSERT EXISTS ( SELECT FROM hive.context WHERE name = 'context' AND current_block_num = 0 AND irreversible_block = 0 AND is_attached = TRUE );
+    ASSERT EXISTS ( SELECT FROM hive.context WHERE name = 'context2' AND current_block_num = 0 AND irreversible_block = 101 AND is_attached = TRUE );
 END
 $BODY$
 ;
