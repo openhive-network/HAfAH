c41ed4bb67993a07c17209b1918f54976f24cfbb
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-05-18 16:09:53 +0200

    functions to create views in separated file

--- src/hive_fork/app_api.sql
+++ src/hive_fork/app_api.sql
@@ -21,39 +21,7 @@ BEGIN
         LEFT JOIN ( SELECT hb.num FROM hive.blocks hb ORDER BY hb.num DESC LIMIT 1 ) as hb ON TRUE
     ;
 
-    EXECUTE format(
-        'DROP VIEW IF EXISTS hive.%s_BLOCKS_VIEW;
-        CREATE VIEW hive.%s_BLOCKS_VIEW
-        AS
-        SELECT
-               hb.num
-             , hb.hash
-             , hb.prev
-             , hb.created_at
-        FROM hive.blocks hb
-        JOIN hive.app_context hc ON  hb.num <= hc.irreversible_block AND hb.num <= hc.current_block_num
-        WHERE hc.name = ''%s''
-        UNION ALL
-        SELECT
-               reversible.num
-             , reversible.hash
-             , reversible.prev
-             , reversible.created_at
-        FROM
-            (
-            SELECT
-            DISTINCT ON (hbr.num) num
-               , hbr.hash
-               , hbr.prev
-               , hbr.created_at
-               , hbr.fork_id
-            FROM hive.blocks_reversible hbr
-            JOIN hive.app_context hc ON  hbr.num > hc.irreversible_block AND hbr.fork_id <= hc.fork_id AND hbr.num <= hc.current_block_num
-            WHERE hc.name = ''%s''
-            ORDER BY hbr.num DESC, hbr.fork_id DESC
-            ) as reversible
-        ;', _name, _name, _name, _name
-    );
+    PERFORM hive.create_blocks_view( _name );
 
 END;
 $BODY$
