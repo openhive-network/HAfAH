3ac75ef74d30f9117e12cfdb5ddabffce4bb3880
kmochocki <kmochocki@syncad.com>
2021-08-12 12:27:31 +0200

    redesigned db connection class holder

--- ah/db/connection.py
+++ ah/db/connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.engine.base import Engine
 class Db:
   def __init__(self, connection_str) -> None:
     self.__db_engine = create_engine(connection_str)
-    self.__db_connection = self.__db_engine.connect()
+    self.open()
 
   def instance(self) -> Engine:
     assert self.__db_connection is not None
@@ -13,3 +13,9 @@ class Db:
 
   def exec(self, query, **kwargs):
     return self.instance().execute(text(query), **kwargs)
+
+  def open(self):
+    self.__db_connection = self.__db_engine.connect()
+
+  def close(self):
+    self.__db_connection.close()
