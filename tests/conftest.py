56ed737ecac3b037d670540b1a39a924bc59047d
kmochocki <kmochocki@syncad.com>
2021-08-13 21:00:52 +0200

    updated address

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import ah.api.endpoints as api_module
 
 @yield_fixture(scope='session')
 def api() -> api_module:
+  # filled with: tests/data/records.sql
   psql_conn_str = "postgresql://postgres:pass@127.0.0.1:5432/hafah_testnet"
   singleton = api_module.backend_singleton(psql_conn_str)
   yield api_module
