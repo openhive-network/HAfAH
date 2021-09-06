f9ee6b1b62647385786b66d8cefd49680751d8ff
Krzysztof Mochocki <kmochocki@syncad.com>
2021-09-06 13:00:37 +0200

    added arguments

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from jsonrpcserver.response import Response
 from ah.api.endpoints import build_methods, backend_singleton
 from aiohttp.web import json_response
 from ah.api.objects import result
+from argparse import ArgumentParser
+from sys import argv
 
-psql_conn_str = "postgresql://postgres:pass@127.0.0.1:5432/hafah"
-ah_singleton = backend_singleton(psql_conn_str)
-methods = build_methods()
+methods = None
 
 def json_encoder( obj ):
   from json import dumps
@@ -41,6 +41,7 @@ def build_error_message(message : str, data : str, status : int) -> Response:
 app = Application()
 
 async def jsonrpc_handler(request : Request):
+    global methods
     try:
       req_as_bin = await request.read()
       req_as_str = req_as_bin.decode('utf-8')
@@ -59,8 +60,18 @@ async def jsonrpc_handler(request : Request):
         data="Details: " + str(ex),
         status=500
       )
-try:
-  app.router.add_post('/', jsonrpc_handler)
-  web.run_app(app, port=8090)
-finally:
-  ah_singleton.finish()
+
+if __name__ == '__main__':
+  engine = ArgumentParser()
+  engine.add_argument('-p, --psql-db-path', dest='psql', type=str, required=True, help='connection string to postgres db ( ex. postgresql://postgres:pass@127.0.0.1:5432/hafah )')
+  engine.add_argument('-n, --port', dest='port', type=int, required=True, help='port to listen on (ex. 6380)')
+  args = engine.parse_args(argv[1:])
+
+  ah_singleton = backend_singleton(args.psql)
+  methods = build_methods()
+
+  try:
+    app.router.add_post('/', jsonrpc_handler)
+    web.run_app(app, port=args.port)
+  finally:
+    ah_singleton.finish()
