d44ded287d5af5e277f1f1dfdd1ce7522a51828c
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-09-08 14:08:39 +0200

    blocks and operations view changed

--- src/hive_fork_manager/blocks_views_for_contexts.sql
+++ src/hive_fork_manager/blocks_views_for_contexts.sql
@@ -39,38 +39,27 @@ AS
 $BODY$
 BEGIN
     EXECUTE format(
-        'DROP VIEW IF EXISTS hive.%s_BLOCKS_VIEW;
-        CREATE VIEW hive.%s_BLOCKS_VIEW
+        'CREATE OR REPLACE VIEW hive.%s_blocks_view
         AS
-        SELECT
-               hb.num
-             , hb.hash
-             , hb.prev
-             , hb.created_at
-        FROM hive.blocks hb
-        JOIN hive.contexts hc ON ( hb.num <= hc.irreversible_block AND hb.num <= hc.current_block_num ) OR NOT hc.is_attached
-        WHERE hc.name = ''%s''
+        SELECT t.num,
+            t.hash,
+            t.prev,
+            t.created_at
+        FROM hive.%s_context_data_view c,
+        LATERAL ( SELECT hb.num,
+            hb.hash,
+            hb.prev,
+            hb.created_at
+           FROM hive.blocks hb
+          WHERE hb.num <= c.min_block
         UNION ALL
-        SELECT
-               reversible.num
-             , reversible.hash
-             , reversible.prev
-             , reversible.created_at
-        FROM
-            (
-            SELECT
-            DISTINCT ON (hbr.num) num
-               , hbr.hash
-               , hbr.prev
-               , hbr.created_at
-               , hbr.fork_id
-            FROM hive.blocks_reversible hbr
-            JOIN hive.contexts hc ON  hbr.num > hc.irreversible_block AND hbr.fork_id <= hc.fork_id AND hbr.num <= hc.current_block_num
-            JOIN hive.registered_tables hrt ON hrt.context_id = hc.id
-            WHERE hc.name = ''%s''
-            ORDER BY hbr.num DESC, hbr.fork_id DESC
-            ) as reversible
-        ;', _context_name, _context_name, _context_name, _context_name
+         SELECT hbr.num,
+            hbr.hash,
+            hbr.prev,
+            hbr.created_at
+           FROM hive.blocks_reversible hbr
+          WHERE c.reversible_range AND hbr.num > c.irreversible_block AND hbr.fork_id <= c.fork_id AND hbr.num <= c.current_block_num) t;
+        ;', _context_name, _context_name
     );
 END;
 $BODY$
@@ -138,48 +127,34 @@ AS
 $BODY$
 BEGIN
 EXECUTE format(
-        'DROP VIEW IF EXISTS hive.%s_OPERATIONS_VIEW;
-        CREATE VIEW hive.%s_OPERATIONS_VIEW
-        AS
-        SELECT
-              ho.id
-            , ho.block_num
-            , ho.trx_in_block
-            , ho.op_pos
-            , ho.op_type_id
-            , ho.body
-        FROM hive.operations ho
-        JOIN hive.contexts hc ON ( ho.block_num <= hc.irreversible_block AND ho.block_num <= hc.current_block_num ) OR NOT hc.is_attached
-        WHERE hc.name = ''%s''
-        UNION ALL
-        SELECT
-              reversible.id
-            , reversible.block_num
-            , reversible.trx_in_block
-            , reversible.op_pos
-            , reversible.op_type_id
-            , reversible.body
-        FROM
-            (
-            SELECT
-                  hor.block_num
-                , hor.id
-                , hor.trx_in_block
-                , hor.op_pos
-                , hor.op_type_id
-                , hor.body
-            FROM hive.operations_reversible hor
-            JOIN (
-               SELECT DISTINCT ON (htr2.block_num) htr2.block_num
-                   , htr2.fork_id
-               FROM hive.transactions_reversible htr2
-               JOIN hive.contexts hc ON htr2.block_num > hc.irreversible_block AND htr2.fork_id <= hc.fork_id AND htr2.block_num <= hc.current_block_num
-               JOIN hive.registered_tables hrt ON hrt.context_id = hc.id
-               WHERE hc.name = ''%s''
-               ORDER BY htr2.block_num DESC, htr2.fork_id DESC
-                ) as forks ON forks.fork_id = hor.fork_id AND forks.block_num = hor.block_num
-            ) as reversible
-        ;', _context_name, _context_name, _context_name, _context_name
+        'CREATE OR REPLACE VIEW hive.%s_operations_view
+         AS
+         SELECT t.id,
+            t.block_num,
+            t.trx_in_block,
+            t.op_pos,
+            t.op_type_id,
+            t.body
+          FROM hive.%s_context_data_view c,
+          LATERAL ( SELECT ho.id,
+                    ho.block_num,
+                    ho.trx_in_block,
+                    ho.op_pos,
+                    ho.op_type_id,
+                    ho.body
+                   FROM hive.operations ho
+                  WHERE ho.block_num <= c.min_block
+                UNION ALL
+                 SELECT hor.id,
+                    hor.block_num,
+                    hor.trx_in_block,
+                    hor.op_pos,
+                    hor.op_type_id,
+                    hor.body
+                    FROM hive.operations_reversible hor
+                    WHERE c.reversible_range AND hor.block_num > c.irreversible_block AND hor.fork_id <= c.fork_id AND hor.block_num <= c.current_block_num
+        ) t
+        ;', _context_name, _context_name
     );
 END;
 $BODY$
