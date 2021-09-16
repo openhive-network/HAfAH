4bd04793a6783fe07516a44c827fa785fe668a5d
Krzysztof Mochocki <kmochocki@syncad.com>
2021-09-16 18:55:02 +0200

    added logging of queries with execution time

--- ah/server/db.py
+++ ah/server/db.py
@@ -102,7 +102,14 @@ class Db:
     async def _query(self, conn, sql, **kwargs):
         """Send a query off to SQLAlchemy."""
         try:
-            return await conn.execute(self._sql_text(sql), **kwargs)
+            sql = str(sqlalchemy.text(sql).\
+              bindparams(**kwargs).\
+              execution_options(autocommit=False).\
+              compile(dialect=sqlalchemy.dialects.postgresql.dialect(), compile_kwargs={"literal_binds": True}))
+            before = perf()
+            result = await conn.execute(sql)
+            print(f'{perf() - before}|{sql}', flush=True)
+            return result
         except Exception as e:
             log.warning("[SQL-ERR] %s in query %s (%s)",
                         e.__class__.__name__, sql, kwargs)
