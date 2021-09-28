8e96b867d1715704c28b46273b5eb425eb2d4190
pczempiel <pczempiel@syncad.com>
2021-09-28 14:51:05 +0200

    Host and port changes.

--- ah/server/serve.py
+++ ah/server/serve.py
@@ -72,11 +72,11 @@ def conf_stdout_custom_file_logger(logger, file_name):
     logger.addHandler(stdout_handler)
     logger.addHandler(file_handler)
 
-def event_loop(runner, host, port):
+def event_loop(runner):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(runner.setup())
-    site = web.TCPSite(runner, host, port)
+    site = web.TCPSite(runner, 'localhost', 8095)
     loop.run_until_complete(site.start())
     loop.run_forever()
 
