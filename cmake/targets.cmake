373f24a718fd5b1bfaba84b7ca3e1fc8613b9dde
mtrela <mtrela@syncad.com>
2021-10-12 09:22:01 +0200

    Cleanup

--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -16,11 +16,47 @@ MACRO( ADD_RUNTIME_LOADED_LIB target_name )
     ADD_POSTGRES_LIBRARIES( ${target_name} )
 ENDMACRO()
 
-MACRO( ADD_STATIC_LIB target_name )
-    FILE( GLOB sources ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp )
-    ADD_LIBRARY( ${target_name}  ${sources} )
+MACRO( ADD_LOADTIME_LOADED_LIB target_name )
+    SET( test_lib test_${target_name} )
+    FILE( GLOB_RECURSE sources ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp )
+    ADD_LIBRARY( ${target_name} MODULE ${sources} )
+    # test lib used by unit tests
+    ADD_LIBRARY( ${test_lib} STATIC ${sources} )
 
-    #ADD_BOOST_LIBRARIES( ${target_name} TRUE )
+    SETUP_COMPILER( ${target_name} )
+    SETUP_COMPILER( ${test_lib} )
+
+    ADD_BOOST_LIBRARIES( ${target_name} TRUE )
+    ADD_BOOST_LIBRARIES( ${test_lib} TRUE )
+
+    ADD_POSTGRES_INCLUDES( ${target_name} )
+    ADD_POSTGRES_INCLUDES( ${test_lib} )
+    ADD_POSTGRES_LIBRARIES( ${target_name} )
+ENDMACRO()
+
+MACRO( ADD_STATIC_LIB target_name )
+    SET( test_lib test_${target_name} )
+    FILE( GLOB_RECURSE sources ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp )
+    ADD_LIBRARY( ${target_name} STATIC ${sources} )
+    # test lib used by unit tests
+    ADD_LIBRARY( ${test_lib} STATIC ${sources} )
+
+    SETUP_COMPILER( ${target_name} )
+    SETUP_COMPILER( ${test_lib} )
+
+    ADD_BOOST_LIBRARIES( ${target_name} TRUE )
+    ADD_BOOST_LIBRARIES( ${test_lib} TRUE )
+
+    ADD_POSTGRES_INCLUDES( ${target_name} )
+    ADD_POSTGRES_INCLUDES( ${test_lib} )
+    ADD_POSTGRES_LIBRARIES( ${target_name} )
+ENDMACRO()
+
+MACRO( PREPARE_STATIC_LIB target_name )
+    FILE( GLOB sources ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp )
+    ADD_LIBRARY( ${target_name} ${sources} )
+
+    ADD_BOOST_LIBRARIES( ${target_name} TRUE )
 
     ADD_POSTGRES_INCLUDES( ${target_name} )
     ADD_POSTGRES_LIBRARIES( ${target_name} )
