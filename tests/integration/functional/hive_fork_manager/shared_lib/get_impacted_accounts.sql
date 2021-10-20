4116aeb4c770fc274dace8fed6cd88fdc8bdacff
mtrela <mtrela@syncad.com>
2021-10-20 07:55:58 +0200

    Additional unit-test

--- tests/integration/functional/hive_fork_manager/shared_lib/get_impacted_accounts.sql
+++ tests/integration/functional/hive_fork_manager/shared_lib/get_impacted_accounts.sql
@@ -21,6 +21,9 @@ $BODY$
 BEGIN
 
     ASSERT ( SELECT COUNT(*) FROM get_impacted_accounts('{"type":"transfer_operation","value":{"from":"admin","to":"steemit","amount":{"amount":"833000","precision":3,"nai":"@@000000021"},"memo":""}}') ) = 2, 'Incorrect impacted accounts';
+
+    ASSERT ( SELECT COUNT(*) FROM get_impacted_accounts('{"type":"escrow_transfer_operation","value":{"from":"xtar","to":"testz","hbd_amount":{"amount":"0","precision":3,"nai":"@@000000013"},"hive_amount":{"amount":"1","precision":3,"nai":"@@000000021"},"escrow_id":123456,"agent":"fabien","fee":{"amount":"1","precision":3,"nai":"@@000000021"},"json_meta":"","ratification_deadline":"2017-02-15T15:15:11","escrow_expiration":"2017-02-16T15:15:11"}}') ) = 3, 'Incorrect impacted accounts';
+
 END;
 $BODY$
 ;
