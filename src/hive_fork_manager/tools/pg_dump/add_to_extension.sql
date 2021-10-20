f680c3f97d1812e70b9df16187bd57014024d3a2
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-10-20 14:32:31 +0200

    scripts which helps to dump hive_fork_manager db

---
+++ src/hive_fork_manager/tools/pg_dump/add_to_extension.sql
@@ -0,0 +1,16 @@
+alter extension hive_fork_manager add table hive.contexts;
+alter extension hive_fork_manager add table hive.registered_tables;
+alter extension hive_fork_manager add table hive.triggers;
+alter extension hive_fork_manager add table hive.events_queue;
+alter extension hive_fork_manager add table hive.fork;
+alter extension hive_fork_manager add table hive.indexes_constraints;
+alter extension hive_fork_manager add table hive.blocks;
+alter extension hive_fork_manager add table hive.irreversible_data;
+alter extension hive_fork_manager add table hive.transactions;
+alter extension hive_fork_manager add table hive.transactions_multisig;
+alter extension hive_fork_manager add table hive.operation_types;
+alter extension hive_fork_manager add table hive.operations;
+alter extension hive_fork_manager add table hive.blocks_reversible;
+alter extension hive_fork_manager add table hive.transactions_reversible;
+alter extension hive_fork_manager add table hive.transactions_multisig_reversible;
+alter extension hive_fork_manager add table hive.operations_reversible;
