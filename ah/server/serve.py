52890eeec887c1fb09cb3d41fd6ac2c6ed52685a
pczempiel <pczempiel@syncad.com>
2021-10-28 17:57:14 +0200

    Handle tests json.

--- ah/server/serve.py
+++ ah/server/serve.py
@@ -6,11 +6,13 @@ import logging
 import time
 import traceback
 import json
+from socketserver  import ThreadingMixIn
+from http.server import BaseHTTPRequestHandler, HTTPServer
 # import asyncio
 from concurrent.futures import ThreadPoolExecutor
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from SocketServer import ThreadingMixIn
 import threading
+import cgi
+
 
 from datetime import datetime
 from time import perf_counter
@@ -28,22 +30,43 @@ from ah.server.db import Db
 
 class Handler(BaseHTTPRequestHandler):
 
-    def do_POST(self):
-        """Process request."""
-        request = self.rfile.read(int(self.headers["Content-Length"])).decode()
-        response = dispatch(request)
-        """Return response."""
+    def _set_headers(self):
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        message =  threading.currentThread().getName()
-        self.wfile.write(message)
-        self.wfile.write(response.encode())
-        self.wfile.write('\n')
-        return
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.get('content-length'))
+        print(threading.currentThread().getName())
+        # self.wfile.write(message)
+        message_string = self.rfile.read(length).decode('utf-8')
+        message = json.loads(message_string) if message_string else None
+        
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(json.dumps(message).encode('utf-8'))
+
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
+    pass
 
 def decimal_serialize(obj):
     return simplejson.dumps(obj=obj, use_decimal=True, default=vars)
@@ -100,71 +123,84 @@ def run_server(db_url, port):
     print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
 
-    """Configure and launch the API server."""
+    """Configure API."""
     log = logging.getLogger(__name__)
     methods = build_methods()
 
-    app = web.Application()
-    app['config'] = dict()
-    app['config']['hive.MAX_DB_ROW_RESULTS'] = 100000
+    app_config = dict()
+    app_config['hive.MAX_DB_ROW_RESULTS'] = 100000
 
     async def init_db(app):
         """Initialize db adapter."""
         app['db'] = await Db.create(db_url)
 
-    async def close_db(app):
-        """Teardown db adapter."""
-        app['db'].close()
-        await app['db'].wait_closed()
+    init_db(app_config)
 
-    app.on_startup.append(init_db)
-    app.on_cleanup.append(close_db)
+    # """Configure and launch the API server."""
+    # log = logging.getLogger(__name__)
+    # methods = build_methods()
 
-    async def jsonrpc_handler(request):
-        """Handles all hive jsonrpc API requests."""
-        t_start = perf_counter()
-        request = await request.text()
+    # app = web.Application()
+    # app['config'] = dict()
+    # app['config']['hive.MAX_DB_ROW_RESULTS'] = 100000
 
-        ctx = {
-          "db": app["db"],
-          "id": json.loads(request)['id'] # TODO: remove this if additional logging is not required
-        }
-        # debug=True refs https://github.com/bcb/jsonrpcserver/issues/71
-        response = None
-        try:
-            response = await dispatch(request, methods=methods, debug=True, context=ctx, serialize=decimal_serialize, deserialize=decimal_deserialize)
-        except Exception as ex:
-            # first log exception
-            # TODO: consider removing this log - potential log spam
-            log.exception(ex)
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            print("*** print_tb:")
-            traceback.print_tb(exc_traceback, limit=1000, file=sys.stdout)
+    # async def init_db(app):
+    #     """Initialize db adapter."""
+    #     app['db'] = await Db.create(db_url)
 
-            # create and send error response
-            error_response = {
-                "jsonrpc":"2.0",
-                "error" : {
-                    "code": -32602,
-                    "data": "Invalid JSON in request: " + str(ex),
-                    "message": "Invalid parameters"
-                },
-                "id" : -1
-            }
-            headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
+    # async def close_db(app):
+    #     """Teardown db adapter."""
+    #     app['db'].close()
+    #     await app['db'].wait_closed()
 
-            return web.json_response(error_response, status=200, headers=headers, dumps=decimal_serialize)
+    # app.on_startup.append(init_db)
+    # app.on_cleanup.append(close_db)
 
-        if response is not None and response.wanted:
-            headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
-            ret = web.json_response(response.deserialized(), status=200, headers=headers, dumps=decimal_serialize)
-            return ret
-        ret = web.Response()
-        return ret
+    # async def jsonrpc_handler(request):
+    #     """Handles all hive jsonrpc API requests."""
+    #     t_start = perf_counter()
+    #     request = await request.text()
+
+    #     ctx = {
+    #       "db": app["db"],
+    #       "id": json.loads(request)['id'] # TODO: remove this if additional logging is not required
+    #     }
+    #     # debug=True refs https://github.com/bcb/jsonrpcserver/issues/71
+    #     response = None
+    #     try:
+    #         response = await dispatch(request, methods=methods, debug=True, context=ctx, serialize=decimal_serialize, deserialize=decimal_deserialize)
+    #     except Exception as ex:
+    #         # first log exception
+    #         # TODO: consider removing this log - potential log spam
+    #         log.exception(ex)
+    #         exc_type, exc_value, exc_traceback = sys.exc_info()
+    #         print("*** print_tb:")
+    #         traceback.print_tb(exc_traceback, limit=1000, file=sys.stdout)
+
+    #         # create and send error response
+    #         error_response = {
+    #             "jsonrpc":"2.0",
+    #             "error" : {
+    #                 "code": -32602,
+    #                 "data": "Invalid JSON in request: " + str(ex),
+    #                 "message": "Invalid parameters"
+    #             },
+    #             "id" : -1
+    #         }
+    #         headers = {
+    #             'Access-Control-Allow-Origin': '*'
+    #         }
+
+    #         return web.json_response(error_response, status=200, headers=headers, dumps=decimal_serialize)
+
+    #     if response is not None and response.wanted:
+    #         headers = {
+    #             'Access-Control-Allow-Origin': '*'
+    #         }
+    #         ret = web.json_response(response.deserialized(), status=200, headers=headers, dumps=decimal_serialize)
+    #         return ret
+    #     ret = web.Response()
+    #     return ret
 
     # app.router.add_post('/', jsonrpc_handler)
     # web.run_app(app, port=port)
