c018a8aaa51932d2bb884899452475c11c4bf7bc
Bartek Wrona <wrona@syncad.com>
2021-11-11 16:34:46 +0100

    - Defined hive.get_balance_impacting_operations() useful to limit number of operations to be processed for balance tracking - Schema of btracker tables extended by block_num, to prepare for read APIs which can query for data specific to given block. - Changed PK of btracker.account_balance_history due to blockchain bugs accepting a self transfer.

--- src/hive_fork_manager/get_impacted_balances.sql
+++ src/hive_fork_manager/get_impacted_balances.sql
@@ -16,3 +16,61 @@ $$;
 CREATE OR REPLACE FUNCTION hive.get_impacted_balances(IN _operation_body text)
 RETURNS SETOF impacted_balances_return
 AS '$libdir/libhfm-@GIT_REVISION@.so', 'get_impacted_balances' LANGUAGE C;
+
+--- Returns set of operations which impact account balances.
+CREATE OR REPLACE FUNCTION hive.get_balance_impacting_operations()
+RETURNS SETOF TEXT
+LANGUAGE plpgsql
+IMMUTABLE
+AS
+$$
+BEGIN
+RETURN QUERY 
+SELECT 'hive::protocol::fill_vesting_withdraw_operation'
+UNION ALL
+SELECT 'hive::protocol::producer_reward_operation'
+UNION ALL
+SELECT 'hive::protocol::claim_account_operation'
+UNION ALL
+SELECT 'hive::protocol::account_create_operation'
+UNION ALL
+SELECT 'hive::protocol::account_create_with_delegation_operation'
+UNION ALL
+SELECT 'hive::protocol::hardfork_hive_restore_operation'
+UNION ALL
+SELECT 'hive::protocol::fill_recurrent_transfer_operation'
+UNION ALL
+SELECT 'hive::protocol::fill_transfer_from_savings_operation'
+UNION ALL
+SELECT 'hive::protocol::liquidity_reward_operation'
+UNION ALL
+SELECT 'hive::protocol::fill_convert_request_operation'
+UNION ALL
+SELECT 'hive::protocol::fill_collateralized_convert_request_operation'
+UNION ALL
+SELECT 'hive::protocol::escrow_transfer_operation'
+UNION ALL
+SELECT 'hive::protocol::escrow_release_operation'
+UNION ALL
+SELECT 'hive::protocol::transfer_operation'
+--UNION ALL -- Ignore in favor to transfer_to_vesting_completed_operation
+--SELECT 'hive::protocol::transfer_to_vesting_operation'
+UNION ALL
+SELECT 'hive::protocol::transfer_to_vesting_completed_operation'
+UNION ALL
+SELECT 'hive::protocol::pow_reward_operation'
+UNION ALL
+SELECT 'hive::protocol::limit_order_create_operation'
+UNION ALL
+SELECT 'hive::protocol::limit_order_create2_operation'
+UNION ALL
+SELECT 'hive::protocol::transfer_to_savings_operation'
+UNION ALL
+SELECT 'hive::protocol::claim_reward_balance_operation'
+UNION ALL
+SELECT 'hive::protocol::proposal_pay_operation'
+UNION ALL
+SELECT 'hive::protocol::sps_convert_operation'
+;
+END
+$$
