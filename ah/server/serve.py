b40ab2b159951423828b708355c20f44b6b487b2
mtrela <mtrela@syncad.com>
2021-11-15 10:29:15 +0100

    Server - v.1

--- ah/server/serve.py
+++ ah/server/serve.py
@@ -12,130 +12,96 @@ from time import perf_counter
 from sqlalchemy.exc import OperationalError
 from aiohttp import web
 from jsonrpcserver.methods import Methods
-from jsonrpcserver import async_dispatch as dispatch
+from jsonrpcserver import dispatch
 
 from ah.api.endpoints import build_methods as account_history
 
 import simplejson
-from ah.server.db import Db
+from ah.server.adapter import Db
 
-# pylint: disable=too-many-lines
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-def decimal_serialize(obj):
-    return simplejson.dumps(obj=obj, use_decimal=True, default=vars)
+from functools import partial
 
-def decimal_deserialize(s):
-    return simplejson.loads(s=s, use_decimal=True)
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = "%(asctime)-15s - %(name)s - %(levelname)s - %(message)s"
+MAIN_LOG_PATH = "ah.log"
 
-async def db_head_state(context):
-    return
-    """Status/health check."""
-    db = context['db']
-    sql = ("SELECT num, created_at, extract(epoch from created_at) ts "
-           "FROM hive_blocks ORDER BY num DESC LIMIT 1")
-    row = await db.query_row(sql)
-    return dict(db_head_block=row['num'],
-                db_head_time=str(row['created_at']),
-                db_head_age=int(time.time() - row['ts']))
+MODULE_NAME = "AH synchronizer"
+logger = logging.getLogger(MODULE_NAME)
+logger.setLevel(LOG_LEVEL)
 
-def build_methods():
-    """Register all supported hive_api/condenser_api.calls."""
-    # pylint: disable=expression-not-assigned, line-too-long
-    methods = Methods()
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(LOG_LEVEL)
+ch.setFormatter(logging.Formatter(LOG_FORMAT))
 
-    # account_history methods
-    methods.add(**account_history())
+fh = logging.FileHandler(MAIN_LOG_PATH)
+fh.setLevel(LOG_LEVEL)
+fh.setFormatter(logging.Formatter(LOG_FORMAT))
 
-    return methods
+if not logger.hasHandlers():
+  logger.addHandler(ch)
+  logger.addHandler(fh)
 
-def truncate_response_log(logger):
-    """Overwrite jsonrpcserver resp logger to truncate output.
+class APIMethods:
 
-    https://github.com/bcb/jsonrpcserver/issues/65 was one native
-    attempt but helps little for more complex response structs.
+  @staticmethod
+  def build_methods():
+      """Register all supported hive_api/condenser_api.calls."""
+      # pylint: disable=expression-not-assigned, line-too-long
+      methods = Methods()
 
-    See also https://github.com/bcb/jsonrpcserver/issues/73.
-    """
-    formatter = logging.Formatter('%(levelname)s:%(name)s:%(message).1024s')
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
+      # account_history methods
+      methods.add(**account_history())
 
-    logger.propagate = False
-    logger.addHandler(handler)
+      return methods
 
-def conf_stdout_custom_file_logger(logger, file_name):
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    file_handler = logging.FileHandler(file_name, 'a', 'utf-8')
+class DBServer:
+  def __init__(self, db_url):
+    self.data = dict()
+    self.db_url = db_url
 
-    logger.addHandler(stdout_handler)
-    logger.addHandler(file_handler)
+  def __enter__(self):
+    logger.info("database is created")
+    self.data['db'] = Db(self.db_url, "root db creation")
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    logger.info("database is closed")
+    self.data['db'].close()
+
+class DBHandler(BaseHTTPRequestHandler):
+  def __init__(self, methods, db_server, *args, **kwargs):
+      self.methods = methods
+      self.db_server = db_server
+      super().__init__(*args, **kwargs)
+
+  @staticmethod
+  def decimal_serialize(obj):
+      return simplejson.dumps(obj=obj, use_decimal=True, default=vars)
+
+  @staticmethod
+  def decimal_deserialize(s):
+      return simplejson.loads(s=s, use_decimal=True)
+
+  def do_POST(self):
+    try:
+      request = self.rfile.read(int(self.headers["Content-Length"])).decode()
+      logger.info(request)
+
+      ctx = {
+        "db": self.db_server.data['db'],
+        "id": json.loads(request)['id'] # TODO: remove this if additional logging is not required
+      }
+      response = dispatch(request, methods=self.methods, debug=True, context=ctx, serialize=DBHandler.decimal_serialize, deserialize=DBHandler.decimal_deserialize)
+    except Exception as ex:
+      logger.error("Exception in POST method: {0}".format(ex))
+      raise ex
 
 def run_server(db_url, port):
-    """Configure and launch the API server."""
+  with DBServer(db_url) as db_server:
+    methods = APIMethods.build_methods()
+    handler = partial(DBHandler, methods, db_server)
 
-    log = logging.getLogger(__name__)
-    methods = build_methods()
-
-    app = web.Application()
-    app['config'] = dict()
-    app['config']['hive.MAX_DB_ROW_RESULTS'] = 100000
-
-    async def init_db(app):
-        """Initialize db adapter."""
-        app['db'] = await Db.create(db_url)
-
-    async def close_db(app):
-        """Teardown db adapter."""
-        app['db'].close()
-        await app['db'].wait_closed()
-
-    app.on_startup.append(init_db)
-    app.on_cleanup.append(close_db)
-
-    async def jsonrpc_handler(request):
-        """Handles all hive jsonrpc API requests."""
-        t_start = perf_counter()
-        request = await request.text()
-        ctx = {
-          "db": app["db"],
-          "id": json.loads(request)['id'] # TODO: remove this if additional logging is not required
-        }
-        # debug=True refs https://github.com/bcb/jsonrpcserver/issues/71
-        response = None
-        try:
-            response = await dispatch(request, methods=methods, debug=True, context=ctx, serialize=decimal_serialize, deserialize=decimal_deserialize)
-        except Exception as ex:
-            # first log exception
-            # TODO: consider removing this log - potential log spam
-            log.exception(ex)
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            print("*** print_tb:")
-            traceback.print_tb(exc_traceback, limit=1000, file=sys.stdout)
-
-            # create and send error response
-            error_response = {
-                "jsonrpc":"2.0",
-                "error" : {
-                    "code": -32602,
-                    "data": "Invalid JSON in request: " + str(ex),
-                    "message": "Invalid parameters"
-                },
-                "id" : -1
-            }
-            headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
-
-            return web.json_response(error_response, status=200, headers=headers, dumps=decimal_serialize)
-
-        if response is not None and response.wanted:
-            headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
-            ret = web.json_response(response.deserialized(), status=200, headers=headers, dumps=decimal_serialize)
-            return ret
-        ret = web.Response()
-        return ret
-
-    app.router.add_post('/', jsonrpc_handler)
-    web.run_app(app, port=port)
+    http_server = HTTPServer(('', port), handler)
+    http_server.serve_forever()
