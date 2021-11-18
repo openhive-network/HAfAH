bbdff4239757320ac2709186d7a7e09bf4d4c07f
mtrela <mtrela@syncad.com>
2021-11-18 16:04:13 +0100

    Acceleration of server - part 2

--- ah/server/serve.py
+++ ah/server/serve.py
@@ -61,35 +61,19 @@ class APIMethods:
 
 class sql_executor:
   def __init__(self, db_url):
-    self.db = None
-    self.db_url = db_url
-
-  def create(self):
     logger.info("database is created")
-    self.db = Db(self.db_url, "root db creation")
+    self.db = Db(db_url, "root db creation")
 
   def close(self):
     logger.info("database is closed")
     self.db.close()
 
-  def clone(self):
-    logger.info("Cloning from root database connection")
-    new_sql_executor    = sql_executor(self.db_url)
-    new_sql_executor.db = self.db.clone("clone db creation")
-
-    return new_sql_executor
-
 class ServerManager:
   def __init__(self, db_url):
     self.db_url       = db_url
-    self.sql_executor = sql_executor(self.db_url)
 
   def __enter__(self):
     logger.info("enter into server manager")
-
-    if self.sql_executor is not None:
-      self.sql_executor.create()
-
     return self
 
   def __exit__(self, exc_type, exc_val, exc_tb):
@@ -126,18 +110,16 @@ class DBHandler(BaseHTTPRequestHandler):
 
       _sql_executor = None
 
-      logger.info( "methods: is {}".format(self.methods) )
+      logger.info( "process id: {}".format(_id) )
 
       if _id in ProcessData.sql_executor:
-        logger.info( "process: is {}".format(os.getpid()) )
         _sql_executor = ProcessData.sql_executor[_id]
       else:
-        logger.info( "process: no {} {}".format(os.getpid(), self.db_server.db_url) )
         _sql_executor = sql_executor(self.db_server.db_url)
-        _sql_executor.create()
         ProcessData.sql_executor[_id] = _sql_executor
 
       assert _sql_executor.db is not None, "lack of database"
+
       ctx = {
         "db": _sql_executor.db,
         "id": json.loads(request)['id'] # TODO: remove this if additional logging is not required
@@ -155,7 +137,9 @@ class DBHandler(BaseHTTPRequestHandler):
   def do_POST(self):
     try:
       request = self.rfile.read(int(self.headers["Content-Length"])).decode()
+
       logger.info(request)
+
       self.send(request)
 
     except Exception as ex:
