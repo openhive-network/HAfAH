07fc639773cd3395de3a7c7abc406a93d560980b
mtrela <mtrela@syncad.com>
2021-11-29 09:40:00 +0100

    Fix in `new_accounts_app`

--- ah/synchronization/haf_new_account_collector.py
+++ ah/synchronization/haf_new_account_collector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#Writing data [block_number;creator;new_account] retrieved from a database into given file name.
+#Getting data [block_number;creator;new_account]
 
 import os
 import json
@@ -10,13 +10,21 @@ from haf_base import application
 
 class callback_handler_accounts:
   
-  def __init__(self, file_name):
+  def __init__(self):
     self.logger = None
     self.app    = None
 
-    self.file_name  = file_name
+    #SQL queries
+    self.create_creator_account_table = '''
+      CREATE SCHEMA IF NOT EXISTS creator_account;
+      CREATE TABLE IF NOT EXISTS creator_account.creation_history
+      (
+        block_num INTEGER NOT NULL,
+        creator_id INTEGER DEFAULT NULL,
+        account_id INTEGER NOT NULL
+      );
+    '''
 
-    #SQL query
     self.get_accounts = '''
       SELECT block_num, body
       FROM hive.operations o
@@ -24,12 +32,22 @@ class callback_handler_accounts:
       WHERE ot.name = 'hive::protocol::account_created_operation' AND block_num >= {} and block_num <= {}
     '''
 
+    self.insert_into_history = []
+    self.insert_into_history.append( "INSERT INTO creator_account.creation_history(block_num, creator_id, account_id) SELECT T.block_num, C.id, A.id FROM ( VALUES" )
+    self.insert_into_history.append( " ( {}, '{}', '{}' )" )
+    self.insert_into_history.append( '''
+      ) T(block_num, creator, new_account)
+      LEFT JOIN hive.accounts C ON T.creator = C.name
+      JOIN hive.accounts A ON T.new_account = A.name
+    ''' )
+
   def checker(self):
     assert self.logger is not None, "a logger must be initialized"
     assert self.app is not None, "an app must be initialized"
 
   def pre_none_ctx(self):
     self.checker()
+    _result = self.app.exec_query(self.create_creator_account_table)
     self.logger.info("Nothing to do: *****PRE-NONE-CTX*****")
 
   def pre_is_ctx(self):
@@ -40,6 +58,22 @@ class callback_handler_accounts:
     self.checker()
     self.logger.info("Nothing to do: *****PRE-ALWAYS*****")
 
+  def execute_complex_query(self, values, q_parts):
+    if len(values) == 0:
+      return
+
+    cnt = 0
+    _total_query = q_parts[0]
+
+    for item in values:
+      _total_query += ( "," if cnt else "" ) + item
+      cnt += 1
+
+    _total_query += q_parts[2]
+
+    print(_total_query)
+    _result = self.app.exec_query(_total_query)
+
   def run(self, low_block, high_block):
     self.checker()
 
@@ -48,7 +82,7 @@ class callback_handler_accounts:
 
     self.logger.info("For blocks {}:{} found {} new accounts".format(low_block, high_block, len(_result)))
 
-    _items = []
+    _values = []
     for record in _result:
       _op = json.loads(record[1])
 
@@ -61,11 +95,9 @@ class callback_handler_accounts:
           _new_account_name = _value['new_account_name']
           if 'creator' in _value:
             _creator = _value['creator']
-            _items.append(str(record[0]) + ';' + _creator + ';' + _new_account_name)
+            _values.append(self.insert_into_history[1].format(record[0], _creator, _new_account_name))
 
-    with open(self.file_name, "a") as f:
-      for item in _items:
-        f.write('{}\n'.format(item))
+    self.execute_complex_query(_values, self.insert_into_history)
 
   def post(self):
     self.checker()
@@ -75,21 +107,20 @@ def process_arguments():
   import argparse
   parser = argparse.ArgumentParser()
 
-  #./haf_base.py -p postgresql://LOGIN:PASSWORD@127.0.0.1:5432/DB_NAME --range-blocks 40000 --file-name "test.txt"
+  #./haf_base.py -p postgresql://LOGIN:PASSWORD@127.0.0.1:5432/DB_NAME --range-blocks 40000
   parser.add_argument("--url", type = str, help = "postgres connection string for AH database")
   parser.add_argument("--range-blocks", type = int, default = 1000, help = "Number of blocks processed at once")
-  parser.add_argument("--file-name", type = str, default = "result.txt", help = "A file where all accounts will be saved")
 
   _args = parser.parse_args()
 
-  return _args.url, _args.range_blocks, _args.file_name
+  return _args.url, _args.range_blocks
 
 def main():
 
-  _url, _range_blocks, file_name = process_arguments()
+  _url, _range_blocks= process_arguments()
 
-  _callbacks      = callback_handler_accounts(file_name)
-  _app            = application(_url, _range_blocks, "all_accounts_app", _callbacks)
+  _callbacks      = callback_handler_accounts()
+  _app            = application(_url, _range_blocks, "new_accounts_app", _callbacks)
   _callbacks.app  = _app
 
   _app.process()
