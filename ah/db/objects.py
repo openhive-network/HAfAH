8894d3b7a41dc9b44f282a5bc0def977cded77fa
Krzysztof Mochocki <kmochocki@syncad.com>
2021-12-14 11:37:49 +0100

    changed serialization from OOP to dicts

--- ah/db/objects.py
+++ ah/db/objects.py
@@ -1,113 +1,114 @@
 from typing import List, Tuple
+from json import loads
 
 
-class result:
-  def __init__( self, result, id : int, jsonrpc : str = "2.0" ):
-    self.jsonrpc = jsonrpc
-    self.result = result
-    self.id = id
+def result(result, id : int, jsonrpc : str = "2.0"):
+  return {
+    "jsonrpc": jsonrpc,
+    "result": result,
+    "id": id
+  }
 
-class operation:
-  def __init__(self, obj : str):
-    from json import loads
-    data = loads(obj)
-    self.type = data["type"]
-    self.value = data["value"]
+def operation(obj : str):
+  data = loads(obj)
+  return {
+    "type": data["type"],
+    "value": data["value"]
+  }
 
-class api_operation:
-  def __init__(self, block : int, obj, *, include_op_id = False):
-    assert obj is not None
-    self.trx_id : str = obj["_trx_id"]
-    self.block : int = obj['_block'] if block is None else block
-    self.trx_in_block : int = obj["_trx_in_block"]
-    self.op_in_trx : int = obj["_op_in_trx"]
-    self.virtual_op : int = int(obj["_virtual_op"])
-    self.timestamp : str = obj["_timestamp"]
-    self.op : operation = operation(obj["_value"])
-    self.operation_id = str(0x8000000000000000 | int(obj["_operation_id"])) if include_op_id else 0
+def api_operation(block : int, obj, *, include_op_id = False):
+  assert obj is not None
+  return {
+    "trx_id" : obj["_trx_id"],
+    "block" : obj['_block'] if block is None else block,
+    "trx_in_block" : obj["_trx_in_block"],
+    "op_in_trx" : obj["_op_in_trx"],
+    "virtual_op" : int(obj["_virtual_op"]),
+    "timestamp" : obj["_timestamp"],
+    "op" : operation(obj["_value"]),
+    "operation_id" : str(0x8000000000000000 | int(obj["_operation_id"])) if include_op_id else 0
+  }
 
-class api_operations_container:
-  item = api_operation
-  def __init__(self, block, iterable : list, *, include_op_id = False):
-      assert iterable is not None
-      self.ops : list = [ api_operations_container.item( block, row, include_op_id=include_op_id ) for row in iterable ]
+def api_operations_container(block, iterable : list, *, include_op_id = False):
+    assert iterable is not None
+    return {
+      "ops": [ api_operation( block, row, include_op_id=include_op_id ) for row in iterable ]
+    }
 
-class transaction:
-  def __init__(self, trx_id, obj):
-    assert obj is not None
-    self.ref_block_num : int = obj['_ref_block_num']
-    self.ref_block_prefix : int = obj['_ref_block_prefix']
-    self.expiration : str = obj['_expiration']
-    self.operations : list = obj['_value']
-    self.extensions : list = []
-    self.signatures : list = [x for x in obj['_signature'] if x is not None]
-    self.transaction_id : str = trx_id
-    self.block_num : str = obj['_block_num']
-    self.transaction_num : str = obj['_trx_in_block']
+def transaction(trx_id, obj):
+  assert obj is not None
+  return {
+    "ref_block_num": obj['_ref_block_num'],
+    "ref_block_prefix": obj['_ref_block_prefix'],
+    "expiration": obj['_expiration'],
+    "operations": obj['_value'],
+    "extensions": [],
+    "signatures": [x for x in obj['_signature'] if x is not None],
+    "transaction_id": trx_id,
+    "block_num": obj['_block_num'],
+    "transaction_num": obj['_trx_in_block']
+  }
 
-class ops_in_block(api_operations_container): pass
-class ops_by_block_wrapper:
-  def __init__(self, iterable: list, block : int, timestamp : str, irreversible : bool):
-      self.ops = iterable
-      self.block = block
-      self.timestamp = timestamp
-      self.irreversible = irreversible
+ops_in_block = api_operations_container
 
-class virtual_ops(api_operations_container):
+def ops_by_block_wrapper(iterable: list, block : int, timestamp : str, irreversible : bool):
+  return {
+    "ops": iterable,
+    "block": block,
+    "timestamp": timestamp,
+    "irreversible": irreversible
+  }
 
-  def __init__(self, irreversible_block : int, iterable: list, last_block : int):
-    super().__init__(None, iterable, include_op_id=True)
-    self.ops_by_block : list = []
-    self.next_block_range_begin : int = 0
-    self.next_operation_begin : int = 0
-    self.__setup_pagination(last_block)
+def virtual_ops(irreversible_block : int, iterable: list, last_block : int):
+  result = api_operations_container(None, iterable, include_op_id=True)
+  ops = result["ops"]
 
-    if irreversible_block is not None:
-      self.__group_by_block(irreversible_block)
-      self.ops.clear()
-
-  def __setup_pagination(self, last_block):
-    if len(self.ops):
-      last_op : api_operation = self.ops[-1]
-      self.next_block_range_begin = last_op.block
-      self.next_operation_begin = int(last_op.operation_id) if last_block < last_op.block else 0
-      self.ops.remove(last_op)
-
-  def __group_by_block(self, irreversible_block):
+  def group_by_block(irreversible_block):
     supp = dict()
-    for item in self.ops:
-      if item.block in supp:
-        supp[item.block].append(item)
-      else:
-        supp[item.block] = [item]
+    ops_by_block = []
 
-    self.ops_by_block = []
+    for item in ops:
+      block_num = item['block']
+      if block_num in supp:
+        supp[block_num].append(item)
+      else:
+        supp[block_num] = [item]
+
     for block, items in supp.items():
-      self.ops_by_block.append( ops_by_block_wrapper(
+      ops_by_block.append( ops_by_block_wrapper(
         iterable=items,
         block=block,
-        timestamp=items[0].timestamp,
+        timestamp=items[0]['timestamp'],
         irreversible=block > irreversible_block
       ))
+    return ops_by_block
+
+  ops_length = len(ops)
+  last_op = ops[-1] if ops_length else {"block": 0, "operation_id": "0"}
+
+  result["next_block_range_begin"] = last_op["block"]
+  result["next_operation_begin"] = int(last_op["operation_id"]) if last_block < last_op["block"] else 0
+  result["ops"].remove(last_op)
+
+  if irreversible_block is not None:
+    result["ops_by_block"] = group_by_block(irreversible_block)
+    result["ops"].clear()
+  else:
+    result["ops_by_block"] = []
 
 
-class account_history_item:
-  def __init__(self, row):
-    self.trx_id : str = row["_trx_id"]
-    self.block : int = row["_block"]
-    self.trx_in_block : int = row["_trx_in_block"]
-    self.op_in_trx : int = row["_op_in_trx"]
-    self.virtual_op : int = int(row["_virtual_op"])
-    self.timestamp : str = row["_timestamp"]
-    self.op : operation = operation( row["_value"] )
-    self.operation_id : int = 0
+def account_history_item(row):
+  return {
+    "trx_id": row["_trx_id"],
+    "block": row["_block"],
+    "trx_in_block": row["_trx_in_block"],
+    "op_in_trx": row["_op_in_trx"],
+    "virtual_op": int(row["_virtual_op"]),
+    "timestamp": row["_timestamp"],
+    "op": operation( row["_value"] ),
+    "operation_id": 0
+  }
 
-class account_history:
-  def __init__(self, iterable):
-      self.history : List[ Tuple[ int, account_history_item ] ] = [ self.__format_item( row ) for row in iterable ]
+def account_history(iterable):
+  return {"history" : [ ( row["_operation_id"], account_history_item( row ) ) for row in iterable ]}
 
-  def __format_item( self, row ) -> Tuple[ int, account_history_item ]:
-    return (
-      row["_operation_id"],
-      account_history_item( row )
-    )
