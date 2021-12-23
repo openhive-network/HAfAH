fd7c060aa1e5b8eb54922855a949040a8daa79ae
Bartek Wrona <wrona@syncad.com>
2021-12-23 12:51:06 +0100

    Modified test scripts due to change command required to create hive_fork_manager extension.

--- tests/integration/functional/hive_fork_manager/test.sh
+++ tests/integration/functional/hive_fork_manager/test.sh
@@ -17,7 +17,7 @@ evaluate_result() {
 
 psql -d postgres -a -f  ./create_db.sql;
 
-psql -d psql_tools_test_db -v ON_ERROR_STOP=on -c 'CREATE EXTENSION hive_fork_manager'
+psql -d psql_tools_test_db -v ON_ERROR_STOP=on -c 'CREATE EXTENSION hive_fork_manager CASCADE;'
 if [ $? -ne 0 ]
 then
   echo "FAILED. Cannot create extension"
