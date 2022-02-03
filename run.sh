05741ee80f746fc21cfb31f2c089ca929f60e813
kristupas <kristupas@pcstransfer.reputeme.com>
2022-02-03 10:14:37 +0200

    Added code for 'hafah_api' schema, user, endpoint creation; config for postgREST; 'run.sh' #14

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo_success() {
+    echo 'SUCCESS: Users and API recreated'
+}
+
+create_user() {
+    psql -a -v "ON_ERROR_STOP=1" -d haf_block_log -c '\timing' -c "call hafah_api.create_api_user();"
+}
+
+create_api() {
+    psql -a -v "ON_ERROR_STOP=1" -d haf_block_log -f $PWD/ah/api/hafah_api.sql
+}
+
+start_webserver() {
+    postgrest webserver.conf
+}
+
+if [ "$1" = "start" ]; then
+    start_webserver
+elif [ "$1" = "re-api" ]; then
+    create_api
+    create_user
+    echo_success
+elif [ "$1" = "re-api-start" ]; then
+    create_api
+    create_user
+    echo_success
+    start_webserver
+fi;
