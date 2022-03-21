2af7e73a1fad9008b314fb189f176335d656f510
Mariusz Trela <mtrela@syncad.com>
2022-03-21 15:07:09 +0100

    Added `replay_accounts_body_operations_filtered_node_with_haf` stage into CI

---
+++ tests/integration/replay_accounts_body_operations_filtered/test_replay_accounts_body_operations_filtered_5milion.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import os
+import sqlalchemy
+from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import sessionmaker
+
+from test_tools import logger
+from tables import EventsQueue, Blocks, Operations, Transactions, TransactionsMultisig, Accounts, AccountOperations
+
+
+BLOCK_LOG_LENGTH = 5_000_000
+
+BLOCKS_COUNT = 5_000_000
+OPERATIONS_COUNT = 9
+TRANSACTIONS_COUNT = 9
+TRANSACTIONS_MULTISIG_COUNT = 0
+ACCOUNTS_COUNT = 3
+ACCOUNT_OPERATIONS_COUNT = 9
+
+
+def test_replay_accounts_body_operations_filtered_5milion():
+    logger.info(f'Start test_replay_accounts_body_operations_filtered_5milion')
+
+    if "DB_URL" not in os.environ:
+        raise Exception('DB_URL environment variable not set')
+
+    url = os.environ.get('DB_URL')
+
+    engine = sqlalchemy.create_engine(url, echo=False, poolclass=NullPool)
+    session = sessionmaker(bind=engine)()
+
+    event = session.query(EventsQueue).filter(EventsQueue.event == 'MASSIVE_SYNC').one()
+    assert event.block_num == BLOCK_LOG_LENGTH
+
+    blocks_count = session.query(Blocks).count()
+    assert blocks_count == BLOCKS_COUNT
+
+    operations_count = session.query(Operations).count()
+    assert operations_count == OPERATIONS_COUNT
+
+    transactions_count = session.query(Transactions).count()
+    assert transactions_count == TRANSACTIONS_COUNT
+
+    transactions_multisig_count = session.query(TransactionsMultisig).count()
+    assert transactions_multisig_count == TRANSACTIONS_MULTISIG_COUNT
+
+    account_count = session.query(Accounts).count()
+    assert account_count == ACCOUNTS_COUNT
+
+    account_operations_count = session.query(AccountOperations).count()
+    assert account_operations_count == ACCOUNT_OPERATIONS_COUNT
