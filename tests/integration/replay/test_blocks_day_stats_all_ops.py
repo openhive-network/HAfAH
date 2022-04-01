dedb6911a88a90c5c0d05ce5ef59f6feb51b1846
Marcin Sobczyk <msobczyk@syncad.com>
2022-04-01 18:24:16 +0200

    move patterns to separate location

--- tests/integration/replay/test_blocks_day_stats_all_ops.py
+++ tests/integration/replay/test_blocks_day_stats_all_ops.py
@@ -11,14 +11,15 @@ def test_blocks_day_stats():
     if "DB_URL" not in os.environ:
         raise Exception('DB_URL environment variable not set')
     url = os.environ.get('DB_URL')
+    patterns_root = Path(os.environ.get('PATTERNS_PATH'))
        
     block_day_all_ops_database = pd.read_sql_table('block_day_stats_all_ops_view', url, schema='hive')
-    pattern_path = Path(__file__).with_name('block_day_stats_all_ops_view.pat.csv')
+    pattern_path = patterns_root.joinpath('block_day_stats_all_ops_view.pat.csv')
     with pattern_path.open('r') as file:
         block_day_all_ops_file = pd.read_csv(file)
 
     try:
         pd.testing.assert_frame_equal(block_day_all_ops_database, block_day_all_ops_file)
     except:
-        block_day_all_ops_database.to_csv('block_day_stats_all_ops_view.out.csv', index=False)
+        block_day_all_ops_database.to_csv(patterns_root.joinpath('block_day_stats_all_ops_view.out.csv'), index=False)
         raise
--- tests/integration/replay/test_replay_5milion.py
+++ tests/integration/replay/test_replay_5milion.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import os
 import sqlalchemy
@@ -8,16 +9,6 @@ from test_tools import logger
 from tables import EventsQueue, Blocks, Operations, Transactions, TransactionsMultisig, Accounts, AccountOperations
 
 
-BLOCK_LOG_LENGTH = 5_000_000
-
-BLOCKS_COUNT = 5_000_000
-OPERATIONS_COUNT = 19_752_015 
-TRANSACTIONS_COUNT = 6_961_192
-TRANSACTIONS_MULTISIG_COUNT = 450
-ACCOUNTS_COUNT = 92_462
-ACCOUNT_OPERATIONS_COUNT = 29_449_321
-
-
 def test_replay_5milion():
     logger.info(f'Start test_replay_5milion')
 
@@ -25,27 +16,30 @@ def test_replay_5milion():
         raise Exception('DB_URL environment variable not set')
 
     url = os.environ.get('DB_URL')
+    patterns_root = Path(os.environ.get('PATTERNS_PATH'))
+    with open(patterns_root.joinpath('haf_rows_count.json')) as f:
+        rows_count = json.load(f)
 
     engine = sqlalchemy.create_engine(url, echo=False, poolclass=NullPool)
     session = sessionmaker(bind=engine)()
 
     event = session.query(EventsQueue).filter(EventsQueue.event == 'MASSIVE_SYNC').one()
-    assert event.block_num == BLOCK_LOG_LENGTH
+    assert event.block_num == rows_count['BLOCK_LOG_LENGTH']
 
     blocks_count = session.query(Blocks).count()
-    assert blocks_count == BLOCKS_COUNT
+    assert blocks_count == rows_count['BLOCKS_COUNT']
 
     operations_count = session.query(Operations).count()
-    assert operations_count == OPERATIONS_COUNT
+    assert operations_count == rows_count['OPERATIONS_COUNT']
 
     transactions_count = session.query(Transactions).count()
-    assert transactions_count == TRANSACTIONS_COUNT
+    assert transactions_count == rows_count['TRANSACTIONS_COUNT']
 
     transactions_multisig_count = session.query(TransactionsMultisig).count()
-    assert transactions_multisig_count == TRANSACTIONS_MULTISIG_COUNT
+    assert transactions_multisig_count == rows_count['TRANSACTIONS_MULTISIG_COUNT']
 
     account_count = session.query(Accounts).count()
-    assert account_count == ACCOUNTS_COUNT
+    assert account_count == rows_count['ACCOUNTS_COUNT']
 
     account_operations_count = session.query(AccountOperations).count()
-    assert account_operations_count == ACCOUNT_OPERATIONS_COUNT
+    assert account_operations_count == rows_count['ACCOUNT_OPERATIONS_COUNT']
