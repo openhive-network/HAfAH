6e78cc3907403e47e20630288c23278263f72259
Krzysztof Mochocki <kmochocki@syncad.com>
2022-04-28 15:24:53 +0200

    Remove scripts for benchmarking

--- run_api.bash
+++ run_api.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DB_URL=$1 							# ex. postgresql://postgres:pass@127.0.0.1:5432/hafah
-PORT=$2									# ex. 8095
+PORT=$2								# ex. 8095
 OUTPUT_LOG=dump.log
 PERFORMANCE_OUTPUT=raw_performance_data.csv
 PROCESSED_PERFORMANCE_OUTPUT=performance.csv
@@ -11,8 +11,8 @@ ROOT_DIR=$PWD
 rm -f $STATS_CSV_OUTPUT $PROCESSED_PERFORMANCE_OUTPUT $OUTPUT_LOG
 
 if [ "$#" -gt 2 ]; then
-    ROOT_DIR=$3
-    echo "setting custom HAfAH root project directory to $ROOT_DIR"
+	ROOT_DIR=$3
+	echo "setting custom HAfAH root project directory to $ROOT_DIR"
 fi
 
 rm -f $OUTPUT_LOG $PERFORMANCE_OUTPUT
@@ -20,6 +20,5 @@ rm -f $OUTPUT_LOG $PERFORMANCE_OUTPUT
 echo 'runnning server...'
 $ROOT_DIR/main.py -p $DB_URL -n $PORT | tee -i $OUTPUT_LOG
 echo 'processing output...'
-$ROOT_DIR/tests/performance/parse_server_output.py $OUTPUT_LOG $PROCESSED_PERFORMANCE_OUTPUT
 grep 'executed in' $OUTPUT_LOG | sed -E 's/.*DEBUG - \[(.*)\] (.*) executed in ([0-9\.,]+)ms/\1|\2|\3/g' > $STATS_CSV_OUTPUT
 echo 'done.'
