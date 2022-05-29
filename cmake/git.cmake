67ccc783ca3286a19645f8bfd9a4f3c86df359fa
mtyszczak <mateusz.tyszczak@gmail.com>
2022-05-29 13:50:20 +0200

    Add git command error code handling in cmake

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -5,9 +5,14 @@ MACRO( GENERATE_GIT_VERSION_FILE )
                 COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
                 WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
                 OUTPUT_VARIABLE GIT_REVISION
+                RESULT_VARIABLE GIT_STATUS
                 ERROR_QUIET
                 OUTPUT_STRIP_TRAILING_WHITESPACE
         )
+        if ( ${GIT_STATUS} AND NOT ${GIT_STATUS} EQUAL 0 )
+          message( FATAL_ERROR "GIT command resulted with error code: ${GIT_STATUS}" )
+        endif()
+
         IF ( "${GIT_REVISION}" STREQUAL "" )
             MESSAGE( FATAL_ERROR "GIT hash could not be retrieved" )
         endif()
