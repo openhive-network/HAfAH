4f6a32c8645188ea56d9a48e767c8dfbf54ffd7e
Krzysztof Mochocki <kmochocki@syncad.com>
2022-06-08 10:56:03 +0200

    Add waiting mechanism for postgres to be up and running

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -euo pipefail 
+set -euo pipefail
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
@@ -8,7 +8,7 @@ cleanup () {
   echo "Performing cleanup...."
   python_pid=$(pidof 'python3')
   echo "python_pid: $python_pid"
-  
+
   kill -INT $python_pid
 
   echo "Waiting for hafah finish..."
@@ -33,6 +33,13 @@ while [ $# -gt 0 ]; do
 done
 
 pushd /home/hafah_user/app
+
+# credits: https://stackoverflow.com/a/39028690/11738218
+RETRIES=12
+until psql ${POSTGRES_URL} -c "SELECT 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 10
+done
 
 ./scripts/setup_postgres.sh --postgres-url=${POSTGRES_URL}
 ./scripts/setup_db.sh --postgres-url=${POSTGRES_URL}
