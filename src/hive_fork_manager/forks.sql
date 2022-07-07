888366c9a814ac28caebb811005142286ec551b5
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-07 18:11:19 +0200

    Add description to columns in table for forks

--- src/hive_fork_manager/forks.sql
+++ src/hive_fork_manager/forks.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS hive.fork(
     id BIGSERIAL NOT NULL,
-    block_num INT NOT NULL,
-    time_of_fork TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    block_num INT NOT NULL, -- head block number, after reverting all blocks from fork (look for `notify_switch_fork` in database.cpp hive project file )
+    time_of_fork TIMESTAMP WITHOUT TIME ZONE NOT NULL, -- time of receiving notification from hived (see: hive.back_from_fork definition)
     CONSTRAINT pk_hive_fork PRIMARY KEY( id )
 );
 
