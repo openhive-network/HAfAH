da1c5a6e0d194fa4f649022a3f7554f4315aa4de
Mariusz <mtrela@syncad.com>
2022-07-14 14:55:57 +0200

    Added `app_context_detach_after_next_block` test

---
+++ tests/integration/functional/hive_fork_manager/app_api/app_context_detach_after_next_block.sql
@@ -0,0 +1,66 @@
+DROP FUNCTION IF EXISTS test_given;
+CREATE FUNCTION test_given()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+    PERFORM hive.app_create_context( 'context' );
+    CREATE SCHEMA A;
+    CREATE TABLE A.table1(id  INTEGER ) INHERITS( hive.context );
+
+    INSERT INTO hive.blocks
+    VALUES
+          ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w' )
+        , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:22-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w' )
+        , ( 3, '\xBADD30', '\xCAFE30', '2016-06-22 19:10:23-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w' )
+        , ( 4, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w' )
+        , ( 5, '\xBADD50', '\xCAFE50', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w' )
+    ;
+    INSERT INTO hive.accounts( id, name, block_num )
+    VALUES (5, 'initminer', 1);
+
+    PERFORM hive.set_irreversible( 5 );
+
+    PERFORM hive.app_next_block( 'context' );
+    PERFORM hive.app_next_block( 'context' );
+    PERFORM hive.app_next_block( 'context' );
+
+    INSERT INTO  A.table1( id ) VALUES ( 66 ),( 67);
+    INSERT INTO  A.table1( id ) VALUES ( 300 ),( 301);
+
+    ASSERT ( SELECT count(*) FROM hive.shadow_a_table1 ) = 4, 'shadow table has to be filled';
+
+    PERFORM hive.app_context_detach( 'context' );
+END;
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_when;
+CREATE FUNCTION test_when()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    VOLATILE
+AS
+$BODY$
+BEGIN
+END
+$BODY$
+;
+
+DROP FUNCTION IF EXISTS test_then;
+CREATE FUNCTION test_then()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+STABLE
+AS
+$BODY$
+BEGIN
+END
+$BODY$
+;
+
+
+
+
