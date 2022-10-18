f57a13ec786a7bf9e25dd0d54d2a783c5ffe14f2
Krzysztof Mochocki <kmochocki@syncad.com>
2022-10-18 10:30:16 +0200

    Fix test by switching from NULL to empty json array

--- tests/integration/functional/hive_fork_manager/hived_api/get_block_3_test.sql
+++ tests/integration/functional/hive_fork_manager/hived_api/get_block_3_test.sql
@@ -123,8 +123,8 @@ BEGIN
     ASSERT __block.extensions = E'[{"version":"1.26"}]'::jsonb, 'Incorrect extensions';
     ASSERT __block.witness_signature = '\x21571234'::bytea, 'Incorrect witness signature';
 
-    __transaction1 = (101, 100, '2016-06-22 19:10:23-07'::timestamp, ARRAY[ 'TWO OPERATION 00', 'TWO OPERATION 01' ], NULL, ARRAY[ '\xBEEF'::bytea, '\xBEEF01'::bytea ]);
-    __transaction2 = (101, 100, '2016-06-22 19:10:23-07'::timestamp, ARRAY[ 'TWO OPERATION 10', 'TWO OPERATION 11' ], NULL, ARRAY[ '\xBEEF0003'::bytea, '\xBEEF000302'::bytea, '\xBEEF000303'::bytea ]);
+    __transaction1 = (101, 100, '2016-06-22 19:10:23-07'::timestamp, ARRAY[ 'TWO OPERATION 00', 'TWO OPERATION 01' ], array_to_json(ARRAY[]::INT[]), ARRAY[ '\xBEEF'::bytea, '\xBEEF01'::bytea ]);
+    __transaction2 = (101, 100, '2016-06-22 19:10:23-07'::timestamp, ARRAY[ 'TWO OPERATION 10', 'TWO OPERATION 11' ], array_to_json(ARRAY[]::INT[]), ARRAY[ '\xBEEF0003'::bytea, '\xBEEF000302'::bytea, '\xBEEF000303'::bytea ]);
 
     ASSERT __block.transactions = Array[ __transaction1, __transaction2 ], 'Incorrect transactions array';
     ASSERT __block.block_id = '\xBADD30'::bytea, 'Incorrect block_id';
