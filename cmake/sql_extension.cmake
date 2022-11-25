8ac7ed6b44a295f0d1c61a100ca19252da04ed23
mzander <jacekz58@wp.pl>
2022-11-25 09:33:00 +0100

    Remove PROXY file

--- cmake/sql_extension.cmake
+++ cmake/sql_extension.cmake
@@ -9,21 +9,13 @@ MACRO( ADD_PSQL_EXTENSION )
 
     SET( extension_path  "${CMAKE_BINARY_DIR}/extensions/${EXTENSION_NAME}" )
 
-    FILE( MAKE_DIRECTORY "${extension_path}" "${extension_path}/update" )
-
     SET( UPDATE_NAME "${EXTENSION_NAME}_update--${HAF_GIT_REVISION_SHA}" )
+
     SET( update_control_script ${UPDATE_NAME}.sql )
-
-    SET( PROXY_UPDATE_VER "UPDATE--${HAF_GIT_REVISION_SHA}" )
-
-    SET( update_path "${extension_path}/update/")
 
     SET( extension_control_file ${EXTENSION_NAME}.control )
 
     SET( extension_control_script ${EXTENSION_NAME}--${HAF_GIT_REVISION_SHA}.sql )
-
-    SET( PROXY_GIT_VER "PROXY--${HAF_GIT_REVISION_SHA}" )
-    SET( PROXY_UPDATE_VER "UPDATE--${HAF_GIT_REVISION_SHA}" )
 
     MESSAGE( STATUS "VERSION: ${HAF_GIT_REVISION_SHA}" )
 
@@ -38,8 +30,7 @@ MACRO( ADD_PSQL_EXTENSION )
     ADD_CUSTOM_COMMAND(
             OUTPUT  "${extension_path}/${extension_control_file}" "${extension_path}/${extension_control_script}"
             COMMAND sed 's/@HAF_GIT_REVISION_SHA@/${HAF_GIT_REVISION_SHA}/g' ${extension_control_file}  > ${extension_path}/${extension_control_file}
-            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/../../cmake/merge_sql.sh ${EXTENSION_DEPLOY_SOURCES} > ${extension_path}/${EXTENSION_NAME}--${PROXY_GIT_VER}.sql
-            COMMAND sed 's/@HAF_GIT_REVISION_SHA@/${HAF_GIT_REVISION_SHA}/g' ${extension_path}/${EXTENSION_NAME}--${PROXY_GIT_VER}.sql  > ${extension_path}/${EXTENSION_NAME}--${HAF_GIT_REVISION_SHA}.sql
+            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/../../cmake/merge_sql.sh ${EXTENSION_DEPLOY_SOURCES} > ${extension_path}/${EXTENSION_NAME}--${HAF_GIT_REVISION_SHA}.sql
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             DEPENDS ${EXTENSION_DEPLOY_SOURCES} ${extension_control_file}
             COMMENT "Generating ${EXTENSION_NAME} files to ${extension_path}"
@@ -47,8 +38,7 @@ MACRO( ADD_PSQL_EXTENSION )
 
     ADD_CUSTOM_COMMAND(
             OUTPUT "${extension_path}/${update_control_script}"
-            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/../../cmake/merge_sql.sh ${EXTENSION_UPDATE_SOURCES} > ${update_path}/${UPDATE_NAME}--${PROXY_UPDATE_VER}.sql
-            COMMAND sed 's/@HAF_GIT_REVISION_SHA@/${HAF_GIT_REVISION_SHA}/g' ${update_path}/${UPDATE_NAME}--${PROXY_UPDATE_VER}.sql  > ${extension_path}/${update_control_script}
+            COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/../../cmake/merge_sql.sh ${EXTENSION_UPDATE_SOURCES} > ${extension_path}/${update_control_script}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
             DEPENDS ${EXTENSION_UPDATE_SOURCES}
             COMMENT "Generating ${EXTENSION_NAME} helper update scripts to ${update_path}, final upgrade script: ${extension_path}/${update_control_script}"
