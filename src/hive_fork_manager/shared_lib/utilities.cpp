openhive-network/haf

Change function name to better match its purpose

src/hive_fork_manager/shared_lib/utilities.cpp
@@ -71,9 +71,9 @@ fc::string get_legacy_style_operation_impl( const hive::protocol::operation& _op
   return fc::json::to_string( _op );
 }
 
-account_name_type get_account_from_accounts_operations_impl( const hive::protocol::operation& _op )
+account_name_type get_creator_from_account_create_operations_impl( const hive::protocol::operation& _op )
 {
-  return hive::app::get_account_from_accounts_operations( _op );
+  return hive::app::get_creator_from_account_create_operations( _op );
 }
 
 flat_set<account_name_type> get_accounts( const char* raw_data, uint32_t data_length )
@@ -337,9 +337,9 @@ void issue_error(const char* msg)
 
 
 
-PG_FUNCTION_INFO_V1(get_account_from_accounts_operations);
+PG_FUNCTION_INFO_V1(get_creator_from_account_create_operations);
 
-Datum get_account_from_accounts_operations(PG_FUNCTION_ARGS)
+Datum get_creator_from_account_create_operations(PG_FUNCTION_ARGS)
 {
   _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
   auto _result = (Datum)0;
@@ -347,7 +347,7 @@ Datum get_account_from_accounts_operations(PG_FUNCTION_ARGS)
   colect_operation_data_and_fill_returned_recordset(
     [=, &_result](const hive::protocol::operation& op)
     {
-      std::string account = get_account_from_accounts_operations_impl( op );
+      std::string account = get_creator_from_account_create_operations_impl( op );
       _result = CStringGetTextDatum( account.c_str() );
     },
     [](){},

src/hive_fork_manager/state_providers/accounts.sql
@@ -33,9 +33,9 @@ END;
 $BODY$
 ;
 
-CREATE OR REPLACE FUNCTION hive.get_account_from_accounts_operations(IN _account_operation hive.operation)
+CREATE OR REPLACE FUNCTION hive.get_creator_from_account_create_operations(IN _account_operation hive.operation)
 RETURNS TEXT
-AS 'MODULE_PATHNAME', 'get_account_from_accounts_operations' LANGUAGE C;
+AS 'MODULE_PATHNAME', 'get_creator_from_account_create_operations' LANGUAGE C;
 
 CREATE OR REPLACE FUNCTION hive.update_state_provider_accounts( _first_block hive.blocks.num%TYPE, _last_block hive.blocks.num%TYPE, _context hive.context_name )
     RETURNS void
@@ -58,7 +58,7 @@ BEGIN
 
     EXECUTE format(
         'INSERT INTO hive.%s_accounts( name )
-        SELECT hive.get_account_from_accounts_operations( ov.body ) as name
+        SELECT hive.get_creator_from_account_create_operations( ov.body ) as name
         FROM hive.%s_operations_view ov
         JOIN hive.operation_types ot ON ov.op_type_id = ot.id
         WHERE

openhive-network/HAfAH

src/hive_fork_manager/shared_lib/utilities.cpp
@@ -77,9 +77,9 @@ fc::string get_legacy_style_operation_impl( const hive::protocol::operation& _op
   return fc::json::to_string( _op );
 }
 
-account_name_type get_account_from_accounts_operations_impl( const hive::protocol::operation& _op )
+account_name_type get_created_from_account_create_operations_impl( const hive::protocol::operation& _op )
 {
-  return hive::app::get_account_from_accounts_operations( _op );
+  return hive::app::get_created_from_account_create_operations( _op );
 }
 
 flat_set<account_name_type> get_accounts( const char* raw_data, uint32_t data_length )
@@ -342,9 +342,9 @@ void issue_error(const char* msg)
 
 
 
-PG_FUNCTION_INFO_V1(get_account_from_accounts_operations);
+PG_FUNCTION_INFO_V1(get_created_from_account_create_operations);
 
-Datum get_account_from_accounts_operations(PG_FUNCTION_ARGS)
+Datum get_created_from_account_create_operations(PG_FUNCTION_ARGS)
 {
   _operation* op = PG_GETARG_HIVE_OPERATION_PP( 0 );
   auto _result = (Datum)0;
@@ -352,7 +352,7 @@ Datum get_account_from_accounts_operations(PG_FUNCTION_ARGS)
   colect_operation_data_and_fill_returned_recordset(
     [=, &_result](const hive::protocol::operation& op)
     {
-      std::string account = get_account_from_accounts_operations_impl( op );
+      std::string account = get_created_from_account_create_operations_impl( op );
       _result = CStringGetTextDatum( account.c_str() );
     },
     [](){},

src/hive_fork_manager/state_providers/accounts.sql
@@ -33,9 +33,9 @@ END;
 $BODY$
 ;
 
-CREATE OR REPLACE FUNCTION hive.get_account_from_accounts_operations(IN _account_operation hive.operation)
+CREATE OR REPLACE FUNCTION hive.get_created_from_account_create_operations(IN _account_operation hive.operation)
 RETURNS TEXT
-AS 'MODULE_PATHNAME', 'get_account_from_accounts_operations' LANGUAGE C;
+AS 'MODULE_PATHNAME', 'get_created_from_account_create_operations' LANGUAGE C;
 
 CREATE OR REPLACE FUNCTION hive.update_state_provider_accounts( _first_block hive.blocks.num%TYPE, _last_block hive.blocks.num%TYPE, _context hive.context_name )
     RETURNS void
@@ -58,7 +58,7 @@ BEGIN
 
     EXECUTE format(
         'INSERT INTO hive.%s_accounts( name )
-        SELECT hive.get_account_from_accounts_operations( ov.body ) as name
+        SELECT hive.get_created_from_account_create_operations( ov.body ) as name
         FROM hive.%s_operations_view ov
         JOIN hive.operation_types ot ON ov.op_type_id = ot.id
         WHERE