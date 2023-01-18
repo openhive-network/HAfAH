a2aeb53fa21d6264182365ca6ce929e05fd79fff
Konrad Botor <kbotor@syncad.com>
2023-01-18 13:49:01 +0100

    CI jobs that build and push official Hive images - ref. #434

---
+++ scripts/build_and_push_instance.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+[[ -z "$SOURCE_DIR" ]] && echo "Variable SOURCE_DIR must be set" && exit 1
+[[ -z "$HAFAH_CI_IMG_BUILDER_PASSWORD" ]] && echo "Variable HAFAH_CI_IMG_BUILDER_PASSWORD must be set" && exit 1
+[[ -z "$HAFAH_CI_IMG_BUILDER_USER" ]] && echo "Variable HAFAH_CI_IMG_BUILDER_USER must be set" && exit 1
+[[ -z "$REGISTRY" ]] && echo "Variable REGISTRY must be set" && exit 1
+
+set -e
+
+SCRIPTPATH=$(dirname "$(realpath "$0")")
+
+"$SCRIPTPATH/build_instance.sh"
+
+echo "$HAFAH_CI_IMG_BUILDER_PASSWORD" | docker login -u "$HAFAH_CI_IMG_BUILDER_USER $REGISTRY" --password-stdin
+
+docker push "$HAFAH_IMAGE_NAME"
+
+echo "HAFAH_IMAGE_NAME=$HAFAH_IMAGE_NAME" > docker_image_name.env
