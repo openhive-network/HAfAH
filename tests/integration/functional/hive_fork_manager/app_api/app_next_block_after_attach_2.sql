d3911c960d77ba3c05f860b814347d49adc1dd3b
Bartek Wrona <wrona@syncad.com>
2023-02-08 20:10:27 +0000

    Fixed broken account name to satisfy blockchain account name rules (inside test: app_next_block_after_attach_2.sql)

--- tests/integration/functional/hive_fork_manager/app_api/app_next_block_after_attach_2.sql
+++ tests/integration/functional/hive_fork_manager/app_api/app_next_block_after_attach_2.sql
@@ -77,7 +77,7 @@ BEGIN
 
     INSERT INTO hive.operations_reversible
     VALUES
-        ( 2, 8, 0, 0, 1, '2016-06-22 19:10:21-07'::timestamp, '{"type":"account_create_operation","value":{"fee":{"amount":"0","precision":3,"nai":"@@000000021"},"creator":"initminer","new_account_name":"account_8_reversible","owner":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"active":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"posting":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"memo_key":"STM7tjB4CNqUD5kbTHdrJUaHE76xicHMQdpD5N32a7wTr1qnSmG1V","json_metadata":"{}"}}' :: hive.operation, 1 );
+        ( 2, 8, 0, 0, 1, '2016-06-22 19:10:21-07'::timestamp, '{"type":"account_create_operation","value":{"fee":{"amount":"0","precision":3,"nai":"@@000000021"},"creator":"initminer","new_account_name":"account_8_rev","owner":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"active":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"posting":{"weight_threshold":1,"account_auths":[],"key_auths":[]},"memo_key":"STM7tjB4CNqUD5kbTHdrJUaHE76xicHMQdpD5N32a7wTr1qnSmG1V","json_metadata":"{}"}}' :: hive.operation, 1 );
 
 
     PERFORM hive.push_block(
