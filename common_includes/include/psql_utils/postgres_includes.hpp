e0161d0cda6c1613818d1190a619086fb4ab84a3
Bartek Wrona <wrona@syncad.com>
2023-02-08 19:18:13 +0000

    PostgreSQL dependencies moved to postgres_includes.hpp common header, including diagnostic pragmas (to disable annoying warnings) like also prevent conflict between Postgres and fc elog macro.

--- common_includes/include/psql_utils/postgres_includes.hpp
+++ common_includes/include/psql_utils/postgres_includes.hpp
@@ -1,20 +1,61 @@
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
+#ifdef elog
+#pragma push_macro( "elog" )
+#undef elog
+#define POP_ELOG
+#endif
+
+//Suppress 'register' keyword usage warning in 3rd party code
+#if defined(__clang__)
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wpadded"
+#elif defined(__GNUC__) || defined(__GNUG__)
+  #pragma GCC diagnostic push 
+  #pragma GCC diagnostic ignored "-Wregister"
+  #pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 extern "C" {
 #include <postgres.h>
+#include <catalog/pg_attribute.h>
+#include <catalog/pg_type.h>
+
 #include <fmgr.h>
+#include <funcapi.h>
+#include <miscadmin.h>
+
 #include <executor/spi.h>
 #include <libpq-fe.h>
 #include <access/sysattr.h>
-#include <catalog/pg_attribute.h>
+
 #include <nodes/makefuncs.h>
-#include <utils/rel.h>
-#include <utils/tuplestore.h>
-#include <utils/lsyscache.h>
+
+#include <utils/array.h>
+#include <utils/builtins.h>
 #include <utils/fmgrprotos.h>
 #include <utils/fmgroids.h>
+#include <utils/jsonb.h>
+#include <utils/lsyscache.h>
+
+#include <utils/rel.h>
+#include <utils/tuplestore.h>
+
 #include <c.h>
+
 }
-#pragma GCC diagnostic pop
+
+#if defined(__clang__)
+  #pragma clang diagnostic pop
+#elif defined(__GNUC__) || defined(__GNUG__)
+  #pragma GCC diagnostic pop
+#endif
+
+#undef elog
+
+#ifdef POP_ELOG
+  #pragma pop_macro( "elog" )
+  #undef POP_ELOG
+#endif 
+
+
