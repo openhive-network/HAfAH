9c7a00f477366c9ae75112a28fa1504fa245efbe
Michał Kudela <kudmich@syncad.com>
2023-02-24 16:03:01 +0100

    Add test based on CI, checks the correct operation of repeated replay - `double HAF replay`

---
+++ tests/test_double_haf_replay.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+set -xeuo pipefail
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+BASE_DIRECTORY=/$(echo "$SCRIPTPATH" | cut -d "/" -f2)
+SOURCE_DATA_DIR=$CI_PROJECT_DIR/data_generated_during_hive_replay
+mkdir -p "$SOURCE_DATA_DIR"
+
+echo "CREATE REPLAY ENVIRONMENT"
+"$SETUP_SCRIPTS_PATH/ci-helpers/prepare_data_and_shm_dir.sh" --data-base-dir="$SOURCE_DATA_DIR" --block-log-source-dir="$BLOCK_LOG_SOURCE_DIR_5M" --config-ini-source="$CONFIG_INI_SOURCE"
+
+echo "START FIRST REPLAY: "
+"$SETUP_SCRIPTS_PATH"/run_hived_img.sh "$HAF_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=haf-instance-5M --replay --stop-replay-at-block=100000 --exit-before-sync --psql-index-threshold=60000 --detach
+echo "Logs from first container hived_instance:"
+docker logs -f haf-instance-5M &
+
+test "$(docker wait haf-instance-5M)" = 0
+"$SETUP_SCRIPTS_PATH"/run_hived_img.sh "$HAF_IMAGE_NAME" --detach --data-dir="$SOURCE_DATA_DIR"/datadir --docker-option=--entrypoint=/home/haf_admin/haf/scripts/psql_run.sh --name=haf-instance-5M
+docker logs -f haf-instance-5M
+
+echo "CHECK THE NUMBER OF REPLAYED BLOCKS"
+cat "$SOURCE_DATA_DIR"/datadir/psql.log
+grep "[ ]100000$" "$SOURCE_DATA_DIR"/datadir/psql.log
+
+echo "START SECOND REPLAY: "
+"$SETUP_SCRIPTS_PATH"/run_hived_img.sh "$HAF_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=haf-instance-5M --replay --stop-replay-at-block=150000 --exit-before-sync --psql-index-threshold=60000 --detach
+echo "Logs from second container hived_instance:"
+docker logs -f haf-instance-5M &
+
+test "$(docker wait haf-instance-5M)" = 0
+"$SETUP_SCRIPTS_PATH"/run_hived_img.sh "$HAF_IMAGE_NAME" --detach --data-dir="$SOURCE_DATA_DIR"/datadir --docker-option=--entrypoint=/home/haf_admin/haf/scripts/psql_run.sh --name=haf-instance-5M
+docker logs -f haf-instance-5M
+
+echo "CHECK THE NUMBER OF REPLAYED BLOCKS"
+cat "$SOURCE_DATA_DIR"/datadir/psql.log
+grep "[ ]150000$" "$SOURCE_DATA_DIR"/datadir/psql.log
+
