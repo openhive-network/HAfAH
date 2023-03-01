openhive-network/haf

restore c++ code for Postgres API
This reverts commit 943383fb "remove psql libraries, they did not support PSQL 12"

common_includes/include/pq_utils/copy_tuples_session.hpp
@@ -1,0 +1,38 @@
+#pragma once
+
+#include "include/pq_utils/copy_session.hpp"
+#include <memory>
+
+extern "C" {
+  struct HeapTupleData;
+  struct tupleDesc;
+  typedef tupleDesc* TupleDesc;
+  typedef HeapTupleData *HeapTuple;
+  typedef struct varlena bytea;
+} // extern "C"
+
+namespace PsqlTools::PostgresPQ {
+
+  class CopyTuplesSession : public CopySession {
+  public:
+    CopyTuplesSession( std::shared_ptr< pg_conn > _connection, const std::string& _table, const std::vector< std::string >& _columns );
+    ~CopyTuplesSession();
+
+    void push_tuple( bytea* _encoded_with_copy_tuple );
+    void push_tuple_as_next_column( const HeapTupleData& _tuple, const TupleDesc& _tupleDesc );
+
+    void push_tuple_header( uint16_t _number_of_fields );
+    void push_null_field() const;
+  private:
+    void push_tuple_to_next_field( const HeapTupleData& _tuple );
+
+  private:
+    void push_tuple_header( const TupleDesc& _tupleDesc );
+    void push_trailing() const;
+
+  private:
+    const uint32_t m_null_field_size;
+    const uint16_t m_trailing_mark;
+  };
+
+} // namespace PsqlTools::PostgresPQ

common_includes/include/psql_utils/spi_session.hpp
@@ -1,0 +1,37 @@
+#pragma once
+
+#include <boost/optional.hpp>
+
+#include <memory>
+
+extern "C" {
+struct tupleDesc;
+typedef tupleDesc* TupleDesc;
+struct HeapTupleData;
+typedef HeapTupleData *HeapTuple;
+} // extern "C"
+
+namespace PsqlTools::PsqlUtils {
+  class SelectResultIterator;
+
+  class ISelectResult {
+  public:
+    virtual const std::string& getQuery() const= 0;
+    virtual TupleDesc getTupleDesc() = 0;
+    virtual boost::optional< HeapTuple > next() = 0;
+  };
+
+  class SpiSession {
+    public:
+      ~SpiSession();
+
+      static std::shared_ptr< SpiSession > create();
+      // read only queries - SELECT ...
+      std::shared_ptr< ISelectResult > executeSelect( std::string _select_query ) const;
+      // utils queries like CREATE TABLE
+      void executeUtil(const std::string& _query ) const;
+    private:
+      SpiSession();
+  };
+
+} // namespace PsqlTools::PsqlUtilsSpi

src/psql_utils/relation_from_name.cpp
@@ -1,0 +1,65 @@
+#include "relation_from_name.hpp"
+
+#include "include/exceptions.hpp"
+#include "relation_wrapper.hpp"
+
+#include <cassert>
+
+using namespace std::string_literals;
+
+namespace PsqlTools::PsqlUtils {
+RelationFromName::RelationFromName( const std::string& _relation_name ) {
+  auto range = makeRangeVar( NULL, const_cast< char* >( _relation_name.c_str() ), -1 );
+
+  if ( range == nullptr ) {
+    THROW_INITIALIZATION_ERROR("Cannot open relation "s + _relation_name);
+  }
+
+  m_postgres_relation = heap_openrv( range, AccessShareLock );
+  if ( m_postgres_relation == nullptr ) {
+    THROW_INITIALIZATION_ERROR("Cannot open relation "s + _relation_name);
+  }
+
+  m_relation_wrapper.reset( new RelationWrapper( m_postgres_relation ) );
+}
+
+RelationFromName::~RelationFromName() {
+
+  m_relation_wrapper.reset();
+
+  heap_close(m_postgres_relation, NoLock);
+}
+
+IRelation::PrimaryKeyColumns
+RelationFromName::getPrimaryKeysColumns() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getPrimaryKeysColumns();
+}
+
+ColumnsIterator
+RelationFromName::getColumns() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getColumns();
+}
+
+std::string
+RelationFromName::createPkeyCondition( bytea* _relation_tuple_in_copy_format ) const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->createPkeyCondition( _relation_tuple_in_copy_format );
+}
+
+std::string
+RelationFromName::createRowValuesAssignment(bytea* _relation_tuple_in_copy_format ) const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->createRowValuesAssignment(_relation_tuple_in_copy_format);
+}
+
+std::string
+RelationFromName::getName() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getName();
+}
+} // namespace PsqlTools::PsqlUtils
+
+
+

src/psql_utils/spi_select_result_iterator.hpp
@@ -1,0 +1,44 @@
+#pragma once
+
+#include "include/psql_utils/spi_session.hpp"
+
+#include <boost/optional.hpp>
+
+#include <memory>
+#include <string>
+
+extern "C" {
+struct tupleDesc;
+typedef tupleDesc* TupleDesc;
+struct HeapTupleData;
+typedef HeapTupleData *HeapTuple;
+} // extern "C"
+
+namespace PsqlTools::PsqlUtils {
+  class SpiSession;
+
+  class SelectResultIterator
+    : public ISelectResult {
+  public:
+    ~SelectResultIterator();
+    SelectResultIterator(const SelectResultIterator& ) = delete;
+    SelectResultIterator(SelectResultIterator&& ) = delete;
+    SelectResultIterator& operator=(const SelectResultIterator& ) = delete;
+    SelectResultIterator& operator=(SelectResultIterator&& ) = delete;
+
+    static std::shared_ptr<SelectResultIterator> create( std::shared_ptr< SpiSession > _session, std::string _query);
+
+    const std::string& getQuery() const override;
+    TupleDesc getTupleDesc() override;
+    boost::optional< HeapTuple > next() override;
+
+  private:
+    SelectResultIterator( std::shared_ptr< SpiSession > _session, std::string _query );
+
+  private:
+    const std::string m_query;
+    std::shared_ptr< SpiSession > m_session;
+    uint32_t m_current_row_id = 0u;
+  };
+
+} // namespace PsqlTools::PsqlUtils

tests/unit/psql_utils/relation_test.cpp
@@ -1,0 +1,146 @@
+#include <boost/test/unit_test.hpp>
+
+#include "include/exceptions.hpp"
+
+#include "relation_from_name.hpp"
+#include "relation_wrapper.hpp"
+
+#include "mock/postgres_mock.hpp"
+
+BOOST_AUTO_TEST_SUITE( relation )
+
+BOOST_AUTO_TEST_CASE( wrapper_negativy_create ) {
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationWrapper( nullptr )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_pkey_columns ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  RelationData raw_relation;
+  raw_relation.rd_id = 123;
+
+  Bitmapset* columns_bitmap = reinterpret_cast< Bitmapset* >(0xAABBCCDDEEFFAABB);
+  const PsqlTools::PsqlUtils::RelationWrapper::PrimaryKeyColumns expected_columns = {1, 3, 8 };
+  static constexpr auto END_OF_BITMAP = -1;
+
+  EXPECT_CALL( *postgres_mock, get_primary_key_attnos( raw_relation.rd_id, true, ::testing::_ ) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( columns_bitmap ) );
+
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, -1 ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[ 0 ] - FirstLowInvalidHeapAttributeNumber  ) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 0 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[1] - FirstLowInvalidHeapAttributeNumber ) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 1 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[2] - FirstLowInvalidHeapAttributeNumber) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 2 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( END_OF_BITMAP ) );
+
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto pk_columns = relation_under_test.getPrimaryKeysColumns();
+
+  BOOST_REQUIRE_EQUAL( pk_columns.size(), 3 );
+  BOOST_REQUIRE( pk_columns == expected_columns );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_name ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  auto expected_table_name = "TEST_TABLE";
+  RelationData raw_relation;
+
+  EXPECT_CALL( *postgres_mock, SPI_getrelname( &raw_relation ) )
+    .Times(1)
+    .WillOnce( ::testing::Return( const_cast<char*>( expected_table_name ) ) )
+  ;
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto table_name = relation_under_test.getName();
+
+  BOOST_REQUIRE_EQUAL( table_name, expected_table_name );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_pkey_columns_no_pk ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  RelationData raw_relation;
+  raw_relation.rd_id = 123;
+
+  EXPECT_CALL( *postgres_mock, get_primary_key_attnos( raw_relation.rd_id, true, ::testing::_ ) )
+          .Times( 1 )
+          .WillOnce( ::testing::Return(  nullptr ) );
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto pk_columns = relation_under_test.getPrimaryKeysColumns();
+
+  BOOST_REQUIRE( pk_columns.empty() );
+}
+
+BOOST_AUTO_TEST_CASE( relation_from_name ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+
+  static constexpr auto RELATION_NAME = "test_relation";
+  Relation postgres_relation_ptr = reinterpret_cast< Relation >(0xAABBCCDDEEFFAABB);
+  RangeVar* range_var_ptr = reinterpret_cast< RangeVar* >(0xFFFFFFFFFFFFFFFF);
+
+  // 1. create varRange
+  EXPECT_CALL( *postgres_mock, makeRangeVar(NULL, ::testing::StrEq( RELATION_NAME ), -1) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( range_var_ptr ) )
+  ;
+  // 2. find the relation by varRange
+  EXPECT_CALL( *postgres_mock, heap_openrv( range_var_ptr, AccessShareLock) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( postgres_relation_ptr ) )
+  ;
+  // 3. close the relation
+  EXPECT_CALL( *postgres_mock, relation_close( postgres_relation_ptr, NoLock) )
+          .Times( 1 )
+ ;
+
+  BOOST_CHECK_NO_THROW(
+    PsqlTools::PsqlUtils::RelationFromName relation_under_test( RELATION_NAME )
+  );
+}
+
+BOOST_AUTO_TEST_CASE( negative_relation_from_name_cannot_varrange_create ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+
+  // 1. cannot create varRange
+  EXPECT_CALL( *postgres_mock, makeRangeVar(::testing::_, ::testing::_, ::testing::_) )
+    .WillRepeatedly( ::testing::Return( nullptr ) )
+  ;
+
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationFromName relation_under_test( "any relation" )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_CASE( negative_relation_from_name_cannot_open_relation ) {
+    auto postgres_mock = PostgresMock::create_and_get();
+
+    RangeVar* range_var_ptr = reinterpret_cast< RangeVar* >(0xFFFFFFFFFFFFFFFF);
+
+    // 1. create varRange
+    EXPECT_CALL( *postgres_mock, makeRangeVar(::testing::_, ::testing::_, ::testing::_) )
+            .Times( 1 )
+            .WillOnce( ::testing::Return( range_var_ptr ) )
+            ;
+    // 2. find the relation by varRange
+    EXPECT_CALL( *postgres_mock, heap_openrv( range_var_ptr, AccessShareLock) )
+            .Times( 1 )
+            .WillOnce( ::testing::Return( nullptr ) )
+            ;
+    // 3. ensure that close wont be called
+    EXPECT_CALL( *postgres_mock, relation_close( ::testing::_, ::testing::_) )
+      .Times( 0 )
+    ;
+
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationFromName relation_under_test( "any relation" )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_SUITE_END()

openhive-network/HAfAH

common_includes/include/pq_utils/copy_tuples_session.hpp
@@ -1,0 +1,38 @@
+#pragma once
+
+#include "include/pq_utils/copy_session.hpp"
+#include <memory>
+
+extern "C" {
+  struct HeapTupleData;
+  struct TupleDescData;
+  typedef TupleDescData* TupleDesc;
+  typedef HeapTupleData *HeapTuple;
+  typedef struct varlena bytea;
+} // extern "C"
+
+namespace PsqlTools::PostgresPQ {
+
+  class CopyTuplesSession : public CopySession {
+  public:
+    CopyTuplesSession( std::shared_ptr< pg_conn > _connection, const std::string& _table, const std::vector< std::string >& _columns );
+    ~CopyTuplesSession();
+
+    void push_tuple( bytea* _encoded_with_copy_tuple );
+    void push_tuple_as_next_column( const HeapTupleData& _tuple, const TupleDesc& _tupleDesc );
+
+    void push_tuple_header( uint16_t _number_of_fields );
+    void push_null_field() const;
+  private:
+    void push_tuple_to_next_field( const HeapTupleData& _tuple );
+
+  private:
+    void push_tuple_header( const TupleDesc& _tupleDesc );
+    void push_trailing() const;
+
+  private:
+    const uint32_t m_null_field_size;
+    const uint16_t m_trailing_mark;
+  };
+
+} // namespace PsqlTools::PostgresPQ

common_includes/include/psql_utils/spi_session.hpp
@@ -1,0 +1,37 @@
+#pragma once
+
+#include <boost/optional.hpp>
+
+#include <memory>
+
+extern "C" {
+struct TupleDescData;
+typedef TupleDescData* TupleDesc;
+struct HeapTupleData;
+typedef HeapTupleData *HeapTuple;
+} // extern "C"
+
+namespace PsqlTools::PsqlUtils {
+  class SelectResultIterator;
+
+  class ISelectResult {
+  public:
+    virtual const std::string& getQuery() const= 0;
+    virtual TupleDesc getTupleDesc() = 0;
+    virtual boost::optional< HeapTuple > next() = 0;
+  };
+
+  class SpiSession {
+    public:
+      ~SpiSession();
+
+      static std::shared_ptr< SpiSession > create();
+      // read only queries - SELECT ...
+      std::shared_ptr< ISelectResult > executeSelect( std::string _select_query ) const;
+      // utils queries like CREATE TABLE
+      void executeUtil(const std::string& _query ) const;
+    private:
+      SpiSession();
+  };
+
+} // namespace PsqlTools::PsqlUtilsSpi

src/psql_utils/relation_from_name.cpp
@@ -1,0 +1,65 @@
+#include "relation_from_name.hpp"
+
+#include "include/exceptions.hpp"
+#include "relation_wrapper.hpp"
+
+#include <cassert>
+
+using namespace std::string_literals;
+
+namespace PsqlTools::PsqlUtils {
+RelationFromName::RelationFromName( const std::string& _relation_name ) {
+  auto range = makeRangeVar( NULL, const_cast< char* >( _relation_name.c_str() ), -1 );
+
+  if ( range == nullptr ) {
+    THROW_INITIALIZATION_ERROR("Cannot open relation "s + _relation_name);
+  }
+
+  m_postgres_relation = table_openrv( range, AccessShareLock );
+  if ( m_postgres_relation == nullptr ) {
+    THROW_INITIALIZATION_ERROR("Cannot open relation "s + _relation_name);
+  }
+
+  m_relation_wrapper.reset( new RelationWrapper( m_postgres_relation ) );
+}
+
+RelationFromName::~RelationFromName() {
+
+  m_relation_wrapper.reset();
+
+  table_close(m_postgres_relation, NoLock);
+}
+
+IRelation::PrimaryKeyColumns
+RelationFromName::getPrimaryKeysColumns() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getPrimaryKeysColumns();
+}
+
+ColumnsIterator
+RelationFromName::getColumns() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getColumns();
+}
+
+std::string
+RelationFromName::createPkeyCondition( bytea* _relation_tuple_in_copy_format ) const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->createPkeyCondition( _relation_tuple_in_copy_format );
+}
+
+std::string
+RelationFromName::createRowValuesAssignment(bytea* _relation_tuple_in_copy_format ) const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->createRowValuesAssignment(_relation_tuple_in_copy_format);
+}
+
+std::string
+RelationFromName::getName() const {
+  assert( m_relation_wrapper );
+  return m_relation_wrapper->getName();
+}
+} // namespace PsqlTools::PsqlUtils
+
+
+

src/psql_utils/spi_select_result_iterator.hpp
@@ -1,0 +1,44 @@
+#pragma once
+
+#include "include/psql_utils/spi_session.hpp"
+
+#include <boost/optional.hpp>
+
+#include <memory>
+#include <string>
+
+extern "C" {
+struct TupleDescData;
+typedef TupleDescData* TupleDesc;
+struct HeapTupleData;
+typedef HeapTupleData *HeapTuple;
+} // extern "C"
+
+namespace PsqlTools::PsqlUtils {
+  class SpiSession;
+
+  class SelectResultIterator
+    : public ISelectResult {
+  public:
+    ~SelectResultIterator();
+    SelectResultIterator(const SelectResultIterator& ) = delete;
+    SelectResultIterator(SelectResultIterator&& ) = delete;
+    SelectResultIterator& operator=(const SelectResultIterator& ) = delete;
+    SelectResultIterator& operator=(SelectResultIterator&& ) = delete;
+
+    static std::shared_ptr<SelectResultIterator> create( std::shared_ptr< SpiSession > _session, std::string _query);
+
+    const std::string& getQuery() const override;
+    TupleDesc getTupleDesc() override;
+    boost::optional< HeapTuple > next() override;
+
+  private:
+    SelectResultIterator( std::shared_ptr< SpiSession > _session, std::string _query );
+
+  private:
+    const std::string m_query;
+    std::shared_ptr< SpiSession > m_session;
+    uint32_t m_current_row_id = 0u;
+  };
+
+} // namespace PsqlTools::PsqlUtils

tests/unit/psql_utils/relation_test.cpp
@@ -1,0 +1,146 @@
+#include <boost/test/unit_test.hpp>
+
+#include "include/exceptions.hpp"
+
+#include "relation_from_name.hpp"
+#include "relation_wrapper.hpp"
+
+#include "mock/postgres_mock.hpp"
+
+BOOST_AUTO_TEST_SUITE( relation )
+
+BOOST_AUTO_TEST_CASE( wrapper_negativy_create ) {
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationWrapper( nullptr )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_pkey_columns ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  RelationData raw_relation;
+  raw_relation.rd_id = 123;
+
+  Bitmapset* columns_bitmap = reinterpret_cast< Bitmapset* >(0xAABBCCDDEEFFAABB);
+  const PsqlTools::PsqlUtils::RelationWrapper::PrimaryKeyColumns expected_columns = {1, 3, 8 };
+  static constexpr auto END_OF_BITMAP = -1;
+
+  EXPECT_CALL( *postgres_mock, get_primary_key_attnos( raw_relation.rd_id, true, ::testing::_ ) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( columns_bitmap ) );
+
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, -1 ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[ 0 ] - FirstLowInvalidHeapAttributeNumber  ) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 0 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[1] - FirstLowInvalidHeapAttributeNumber ) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 1 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( expected_columns[2] - FirstLowInvalidHeapAttributeNumber) );
+  EXPECT_CALL( *postgres_mock, bms_next_member( columns_bitmap, expected_columns[ 2 ] - FirstLowInvalidHeapAttributeNumber ) ).Times( 1 ).WillOnce( ::testing::Return( END_OF_BITMAP ) );
+
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto pk_columns = relation_under_test.getPrimaryKeysColumns();
+
+  BOOST_REQUIRE_EQUAL( pk_columns.size(), 3 );
+  BOOST_REQUIRE( pk_columns == expected_columns );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_name ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  auto expected_table_name = "TEST_TABLE";
+  RelationData raw_relation;
+
+  EXPECT_CALL( *postgres_mock, SPI_getrelname( &raw_relation ) )
+    .Times(1)
+    .WillOnce( ::testing::Return( const_cast<char*>( expected_table_name ) ) )
+  ;
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto table_name = relation_under_test.getName();
+
+  BOOST_REQUIRE_EQUAL( table_name, expected_table_name );
+}
+
+BOOST_AUTO_TEST_CASE( wrapper_get_pkey_columns_no_pk ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+  RelationData raw_relation;
+  raw_relation.rd_id = 123;
+
+  EXPECT_CALL( *postgres_mock, get_primary_key_attnos( raw_relation.rd_id, true, ::testing::_ ) )
+          .Times( 1 )
+          .WillOnce( ::testing::Return(  nullptr ) );
+
+  PsqlTools::PsqlUtils::RelationWrapper relation_under_test( &raw_relation );
+
+  auto pk_columns = relation_under_test.getPrimaryKeysColumns();
+
+  BOOST_REQUIRE( pk_columns.empty() );
+}
+
+BOOST_AUTO_TEST_CASE( relation_from_name ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+
+  static constexpr auto RELATION_NAME = "test_relation";
+  Relation postgres_relation_ptr = reinterpret_cast< Relation >(0xAABBCCDDEEFFAABB);
+  RangeVar* range_var_ptr = reinterpret_cast< RangeVar* >(0xFFFFFFFFFFFFFFFF);
+
+  // 1. create varRange
+  EXPECT_CALL( *postgres_mock, makeRangeVar(NULL, ::testing::StrEq( RELATION_NAME ), -1) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( range_var_ptr ) )
+  ;
+  // 2. find the relation by varRange
+  EXPECT_CALL( *postgres_mock, table_openrv( range_var_ptr, AccessShareLock) )
+    .Times( 1 )
+    .WillOnce( ::testing::Return( postgres_relation_ptr ) )
+  ;
+  // 3. close the relation
+  EXPECT_CALL( *postgres_mock, table_close( postgres_relation_ptr, NoLock) )
+          .Times( 1 )
+ ;
+
+  BOOST_CHECK_NO_THROW(
+    PsqlTools::PsqlUtils::RelationFromName relation_under_test( RELATION_NAME )
+  );
+}
+
+BOOST_AUTO_TEST_CASE( negative_relation_from_name_cannot_varrange_create ) {
+  auto postgres_mock = PostgresMock::create_and_get();
+
+  // 1. cannot create varRange
+  EXPECT_CALL( *postgres_mock, makeRangeVar(::testing::_, ::testing::_, ::testing::_) )
+    .WillRepeatedly( ::testing::Return( nullptr ) )
+  ;
+
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationFromName relation_under_test( "any relation" )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_CASE( negative_relation_from_name_cannot_open_relation ) {
+    auto postgres_mock = PostgresMock::create_and_get();
+
+    RangeVar* range_var_ptr = reinterpret_cast< RangeVar* >(0xFFFFFFFFFFFFFFFF);
+
+    // 1. create varRange
+    EXPECT_CALL( *postgres_mock, makeRangeVar(::testing::_, ::testing::_, ::testing::_) )
+            .Times( 1 )
+            .WillOnce( ::testing::Return( range_var_ptr ) )
+            ;
+    // 2. find the relation by varRange
+    EXPECT_CALL( *postgres_mock, table_openrv( range_var_ptr, AccessShareLock) )
+            .Times( 1 )
+            .WillOnce( ::testing::Return( nullptr ) )
+            ;
+    // 3. ensure that close wont be called
+    EXPECT_CALL( *postgres_mock, table_close( ::testing::_, ::testing::_) )
+      .Times( 0 )
+    ;
+
+  BOOST_CHECK_THROW(
+      PsqlTools::PsqlUtils::RelationFromName relation_under_test( "any relation" )
+    , PsqlTools::ObjectInitializationException
+  );
+}
+
+BOOST_AUTO_TEST_SUITE_END()