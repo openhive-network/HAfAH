a1c2f6b5232e3dd050ceb8c879887b8627f2c01d
Bartek Wrona <wrona@syncad.com>
2023-03-12 22:12:24 +0000

    Removed ADD_BOOST_LIBRARIES macro in favor to centralized Boost handling provided by ADD_TARGET_BOOST_LIBRARIES provided by Hived submodule.

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -1,26 +1,3 @@
-MACRO( ADD_BOOST_LIBRARIES target_name static_library )
-    SET( BOOST_COMPONENTS )
-    LIST( APPEND BOOST_COMPONENTS
-            thread
-            date_time
-            system
-            filesystem
-            program_options
-            serialization
-            unit_test_framework
-            context locale iostreams
-    )
-    IF( ${static_library} )
-      SET( Boost_USE_STATIC_LIBS ON CACHE STRING "ON or OFF" FORCE )
-    else()
-      SET( Boost_USE_STATIC_LIBS OFF CACHE STRING "ON or OFF" FORCE )
-    endif()
-
-    FIND_PACKAGE( Boost 1.53 REQUIRED COMPONENTS ${BOOST_COMPONENTS} )
-
-    TARGET_LINK_LIBRARIES( ${target_name} PRIVATE ${Boost_LIBRARIES} )
-ENDMACRO()
-
 MACRO( ADD_POSTGRES_LIBRARIES target_name )
     FIND_LIBRARY(PQ_LIB pq)
     IF ( PQ_LIB )
