ca1379fb74040c5365b8943b62af71b79b9f23fd
Bartek Wrona <wrona@syncad.com>
2023-04-12 17:41:49 +0200

    Root CMakeLists.txt also affects last HAF commit.

--- scripts/ci-helpers/retrieve_last_commit.sh
+++ scripts/ci-helpers/retrieve_last_commit.sh
@@ -3,7 +3,7 @@
 P="${1}"
 pushd "$P" >/dev/null 2>&1
 # this list is used to detect changes affecting hived binaries, list might change in future
-COMMIT=$(git log --pretty=format:"%H" -- hive/ src/ cmake/ scripts/ docker/ Dockerfile | head -1)
+COMMIT=$(git log --pretty=format:"%H" -- hive/ src/ cmake/ scripts/ docker/ Dockerfile CMakeLists.txt | head -1)
 
 popd >/dev/null 2>&1
 
