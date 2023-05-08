1255b7d99e295ea15992f5d0a82d6cf71598d039
Michał Kudela <kudmich@syncad.com>
2023-05-08 09:56:58 +0200

    Change the helper script used to get the headblock

--- scripts/psql_run.sh
+++ scripts/psql_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Helper script associated with the test `/haf/tests/integration/test_double_haf_replay.sh`
+
 set -xeuo pipefail
 
 LOG_FILE="${DATADIR}/${LOG_FILE:-psql.log}"
@@ -7,7 +9,26 @@ sudo -n touch $LOG_FILE
 sudo -n chown -Rc hived:users $LOG_FILE
 sudo -n chmod a+rw "$LOG_FILE"
 
-sudo -n /etc/init.d/postgresql restart
+POSTGRESLOG=$(find /var/log -name postgresql*.log) || true
+echo $POSTGRESLOG || true
+sudo ls -lah $POSTGRESLOG || true
+echo "mtlk Listing 10 000 last lines of postgres log"
+sudo tail -n 10000 $POSTGRESLOG
+echo "mtlk end listing postgres log"
+
+# credits: https://stackoverflow.com/a/39028690/11738218
+RETRIES=12
+until psql -d haf_block_log -c "SELECT 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 10
+done
+
+POSTGRESLOG=$(find / -name postgresql*.log) || true
+echo $POSTGRESLOG || true
+sudo ls -lah $POSTGRESLOG || true
+echo "mtlk Listing 10 000 last lines of postgres log"
+sudo tail -n 10000 $POSTGRESLOG
+echo "mtlk end listing postgres log"
 
 echo "CHECK NUMBER OF REPLAYED BLOCKS"
 psql -d haf_block_log -c 'select count (*) from hive.blocks' 2>&1 | tee -i ${LOG_FILE}
