549e39c094c28b83fcab8a7535eed189a6cecf5f
Krzysztof Leśniak <klesniak@syncad.com>
2023-07-05 16:48:00 +0200

    Add tests for converting operation to bytea text

---
+++ tests/integration/functional/hive_fork_manager/shared_lib/operation_to_text_conversion.sql
@@ -0,0 +1,43 @@
+DROP FUNCTION IF EXISTS haf_admin_test_when;
+CREATE FUNCTION haf_admin_test_when()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    VOLATILE
+AS
+$BODY$
+BEGIN
+  ASSERT (select '{"type":"system_warning_operation","value":{"message":""}}'::jsonb::hive.operation::text = '\x5200');
+
+  ASSERT (select '{"type":"system_warning_operation","value":{"message":"abc"}}'::jsonb::hive.operation::text = '\x5203616263');
+
+  ASSERT (select '{"type":"limit_order_cancel_operation","value":{"owner":"complexring","orderid":4294967295}}'::jsonb::hive.operation::text = '\x060b636f6d706c657872696e67ffffffff');
+
+  ASSERT (select '{"type":"system_warning_operation","value":{"message":"no impacted accounts"}}'::jsonb::hive.operation::text = '\x52146e6f20696d706163746564206163636f756e7473');
+
+  ASSERT (select '{
+    "type": "pow_operation",
+    "value": {
+        "work": {
+            "work": "000000049711861bce6185671b672696eca64398586a66319eacd875155b77fc",
+            "input": "c55811a1a9cf6a281acad3aba38223027158186cfd280c41fffe5e2b0d2d6e0b",
+            "worker": "STM6tC4qRjUPKmkqkug5DvSgkeND5DHhnfr3XTgpp4b4nejMEwn9k",
+            "signature": "1fbce97f375ac58c185905ac8e44a9c8b50b7e618bf4a7559816d8316e3b09ff54da096c2f5eddcca1229cf0b9da9597eac2ae676e424bdb432a7855295cd81a00"
+        },
+        "nonce": 42,
+        "props": {
+            "hbd_interest_rate": 1000,
+            "maximum_block_size": 131072,
+            "account_creation_fee": {
+                "nai": "@@000000021",
+                "amount": "100000",
+                "precision": 3
+            }
+        },
+        "block_id": "00015d56d6e721ede5aad1babb0fe818203cbeeb",
+        "worker_account": "sminer10"
+    }
+}'::jsonb::hive.operation::text =
+'\x0e08736d696e6572313000015d56d6e721ede5aad1babb0fe818203cbeeb2a000000000000000306b7270831d7e89a5d2b23ba614e6af9f587d2916cbd8f5fd736faa08acdda1ac55811a1a9cf6a281acad3aba38223027158186cfd280c41fffe5e2b0d2d6e0b1fbce97f375ac58c185905ac8e44a9c8b50b7e618bf4a7559816d8316e3b09ff54da096c2f5eddcca1229cf0b9da9597eac2ae676e424bdb432a7855295cd81a00000000049711861bce6185671b672696eca64398586a66319eacd875155b77fca08601000000000003535445454d000000000200e803');
+END;
+$BODY$
+;
