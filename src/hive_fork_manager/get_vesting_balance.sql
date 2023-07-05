f7b58017f82b2b1441062ab81b6eb69752186a54
mzander <jacekz58@wp.pl>
2023-07-05 09:08:06 +0200

    Add function calculating how much HIVE are worth per vest at current block

---
+++ src/hive_fork_manager/get_vesting_balance.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION hive.get_vesting_balance(_block_num INT, _current_vesting_shares BIGINT)
+    RETURNS numeric
+    LANGUAGE plpgsql
+    STABLE
+AS
+$BODY$
+DECLARE 
+	__total_vesting_shares numeric;
+	__total_vesting_fund_hive numeric;
+	__hp BIGINT;
+BEGIN				
+
+	SELECT total_vesting_shares, total_vesting_fund_hive 
+	INTO __total_vesting_shares, __total_vesting_fund_hive
+	FROM hive.blocks WHERE num= _block_num;
+	
+    __hp := (_current_vesting_shares * __total_vesting_fund_hive) / NULLIF(__total_vesting_shares, 0);
+	
+	RETURN __hp;
+END;
+$BODY$
+;
+
+
