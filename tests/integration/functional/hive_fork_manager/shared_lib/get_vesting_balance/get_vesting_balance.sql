f4cd79f3e3ef8ae3806816c13b30200328a20363
mzander <jacekz58@wp.pl>
2023-07-05 09:08:53 +0200

    Add unit test for get_vesting_balance

---
+++ tests/integration/functional/hive_fork_manager/shared_lib/get_vesting_balance/get_vesting_balance.sql
@@ -0,0 +1,37 @@
+DROP FUNCTION IF EXISTS haf_admin_test_given;
+CREATE FUNCTION haf_admin_test_given()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+VOLATILE
+AS
+$BODY$
+BEGIN
+	INSERT INTO hive.blocks
+    VALUES
+          ( 1, '\xBADD10', '\xCAFE10', '2016-06-22 19:10:21-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 312785920, 5435823, 1000, 1000, 1000, 2000, 2000 )
+        , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 312785920, 0, 1000, 1000, 1000, 2000, 2000 )
+    ;
+
+    INSERT INTO hive.accounts( id, name, block_num )
+    VALUES (5, 'initminer', 1)
+    ;
+
+END;
+$BODY$
+;
+
+
+DROP FUNCTION IF EXISTS haf_admin_test_then;
+CREATE FUNCTION haf_admin_test_then()
+    RETURNS void
+    LANGUAGE 'plpgsql'
+STABLE
+AS
+$BODY$
+BEGIN
+    ASSERT (SELECT hive.get_vesting_balance(1, 12312213124132120)) = 708464368554262962, 'Results do not match';
+    ASSERT (SELECT hive.get_vesting_balance(2, 12312312412)) IS NULL, 'Function did not return NULL';
+END;
+$BODY$
+;
+
