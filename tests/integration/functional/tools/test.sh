640bb2955a2715a1dedd94f28f4989c09a2abfbd
Krzysztof Leśniak <klesniak@syncad.com>
2023-07-05 15:55:57 +0200

    Get rid of EXCEPTION WHEN undefined_function in functional tests
    The testing framework ignored errors related to calling undefined
    functions. This is great, because tests don't need to provide empty
    functions for all combination of {haf_admin,test_hived,alice,bob}_test_{given,when,error,then}.
    
    This creates a possible issue if the test calls an undefined function
    (because of a typo or lack of namespace for example). In this case test
    is reported as passed where in fact it isn't. This makes it easy to
    write a test that passes by accident.
    
    Instead of ignoring undefined_function exception, predefine all
    function combinations before loading test file. These functions are
    defined to raise an error for *_error case and to be empty for other
    cases. Test script can then replace required functions as needed.

--- tests/integration/functional/tools/test.sh
+++ tests/integration/functional/tools/test.sh
@@ -14,27 +14,42 @@ trap on_exit EXIT;
 psql -p $postgres_port -d $DB_NAME -a -v ON_ERROR_STOP=on -f  ./tools/test_tools.sql;
 evaluate_result $?
 
+users="haf_admin test_hived alice bob"
+tests="given when error then"
+
+for testfun in ${tests}; do
+  for user in ${users}; do
+    if [ "${testfun}" = "error" ]; then
+      body="raise exception 'Expected to fail';"
+    else
+      body=""
+    fi
+    query="
+CREATE OR REPLACE FUNCTION ${user}_test_${testfun}()
+RETURNS void
+LANGUAGE plpgsql
+VOLATILE
+AS
+\$\$
+BEGIN
+$body
+END
+\$\$;"
+    psql -p $postgres_port -d $DB_NAME -v ON_ERROR_STOP=on -c "$query"
+    evaluate_result $?
+  done
+done
+
 # add test functions:
 # load tests function
 psql -p $postgres_port -d $DB_NAME -a -v ON_ERROR_STOP=on -f  ${test_path};
 evaluate_result $?
 
-users="haf_admin test_hived alice bob"
-tests="given when error then"
-
 # you can use alice_test_given, alice_test_when, alice_test_error, alice_test_then and their bob's and test_hived equivalents
 
 for testfun in ${tests}; do
   for user in ${users}; do
-    sql_code_no_error="DO \$\$
-    BEGIN
-      BEGIN
-        PERFORM ${user}_test_${testfun}();
-      EXCEPTION WHEN undefined_function THEN
-      END;
-    END \$\$;"
-
-    sql_code_error="SELECT ${user}_test_${testfun}();";
+    query="SELECT ${user}_test_${testfun}();";
 
     if [ "$user" =  "haf_admin" ]; then
       pg_call="-p $postgres_port -d $DB_NAME -v ON_ERROR_STOP=on -c"
@@ -43,10 +58,10 @@ for testfun in ${tests}; do
     fi
 
     if [ "${testfun}" = "error" ]; then
-      psql ${pg_call} "${sql_code_error}";
+      psql ${pg_call} "${query}";
       evaluate_error_result $?
     else
-      psql ${pg_call} "${sql_code_no_error}";
+      psql ${pg_call} "${query}";
       evaluate_result $?
     fi
   done
