ecf66cecc5214c39cd45c28652710dc8ba4c939d
mzander <jacekz58@wp.pl>
2023-07-24 10:21:27 +0200

    Add to get_vesting_balance unit test reversible block verification

--- tests/integration/functional/hive_fork_manager/shared_lib/get_vesting_balance/get_vesting_balance.sql
+++ tests/integration/functional/hive_fork_manager/shared_lib/get_vesting_balance/get_vesting_balance.sql
@@ -12,6 +12,12 @@ BEGIN
         , ( 2, '\xBADD20', '\xCAFE20', '2016-06-22 19:10:24-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 312785920, 0, 1000, 1000, 1000, 2000, 2000 )
     ;
 
+    INSERT INTO hive.blocks_reversible
+    VALUES
+           ( 3, '\xBADD40', '\xCAFE40', '2016-06-22 19:10:25-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 312785920, 5435823, 1000, 1000, 1000, 2000, 2000, 1 )
+         , ( 4, '\xBADD5A', '\xCAFE5A', '2016-06-22 19:10:55-07'::timestamp, 5, '\x4007', E'[]', '\x2157', 'STM65w', 1000, 312785920, 5435823, 1000, 1000, 1000, 2000, 2000, 1 )
+    ;
+
     INSERT INTO hive.accounts( id, name, block_num )
     VALUES (5, 'initminer', 1)
     ;
@@ -29,8 +35,10 @@ STABLE
 AS
 $BODY$
 BEGIN
-    ASSERT (SELECT hive.get_vesting_balance(1, 12312213124132120)) = 708464368554262962, 'Results do not match';
+    ASSERT (SELECT hive.get_vesting_balance(1, 12312213124132120)) = 708464368554262962, 'Results on block 1 do not match';
     ASSERT (SELECT hive.get_vesting_balance(2, 12312312412)) IS NULL, 'Function did not return NULL';
+    ASSERT (SELECT hive.get_vesting_balance(3, 12312213124132120)) = 708464368554262962, 'Results on block 3 do not match';
+    ASSERT (SELECT hive.get_vesting_balance(4, 12312213124132120)) = 708464368554262962, 'Results on block 4 do not match';
 END;
 $BODY$
 ;
