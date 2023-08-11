d257e678ae0076aad04edb00d056858b740b56bb
Krzysztof Leśniak <klesniak@syncad.com>
2023-08-11 12:10:08 +0200

    Tests for call_cxx

--- tests/unit/psql_utils/call_cxx.cpp
+++ tests/unit/psql_utils/call_cxx.cpp
@@ -6,13 +6,72 @@
 
 BOOST_FIXTURE_TEST_SUITE( call_cxx_tests, GmockFixture )
 
-BOOST_AUTO_TEST_CASE( foo )
+BOOST_AUTO_TEST_CASE( call_cxx_allows_to_set_value_if_no_error )
 {
   int res = 0;
   PsqlTools::PsqlUtils::call_cxx([&res](){
     res = 7;
   });
   BOOST_CHECK(res == 7);
+}
+
+BOOST_AUTO_TEST_CASE( std_runtime_error_is_converted_to_postgres_error )
+{
+  int res = 0;
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([&res](){
+    throw std::runtime_error("some error");
+  }));
+  BOOST_CHECK(res == 0);
+}
+
+BOOST_AUTO_TEST_CASE( std_logic_error_is_converted_to_postgres_error )
+{
+  int res = 0;
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([&res](){
+    throw std::logic_error("some error");
+  }));
+  BOOST_CHECK(res == 0);
+}
+
+BOOST_AUTO_TEST_CASE( fc_exception_is_converted_to_postgres_error )
+{
+  int res = 0;
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([&res](){
+    FC_THROW_EXCEPTION( fc::assert_exception, "test exception" );
+  }));
+  BOOST_CHECK(res == 0);
+}
+
+BOOST_AUTO_TEST_CASE( exception_sets_the_passed_errcode )
+{
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([](){
+    throw std::runtime_error("some error");
+  }, ERRCODE_INVALID_TEXT_REPRESENTATION));
+  BOOST_CHECK(geterrcode() == ERRCODE_INVALID_TEXT_REPRESENTATION);
+
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([](){
+    throw std::runtime_error("some error");
+  }, ERRCODE_INVALID_BINARY_REPRESENTATION));
+  BOOST_CHECK(geterrcode() == ERRCODE_INVALID_BINARY_REPRESENTATION);
+}
+
+BOOST_AUTO_TEST_CASE( postgres_error_is_caught )
+{
+  int res = 0;
+  EXPECT_PG_ERROR(PsqlTools::PsqlUtils::call_cxx([&res](){
+    MAKE_POSTGRES_ERROR;
+  }));
+  BOOST_CHECK(res == 0);
+}
+
+BOOST_AUTO_TEST_CASE( postgres_warning_is_not_an_error )
+{
+  int res = 0;
+  PsqlTools::PsqlUtils::call_cxx([&res](){
+    ereport( WARNING, ( errcode( ERRCODE_DATA_EXCEPTION ), errmsg( "%s", "warning") ) );
+    res = 8;
+  });
+  BOOST_CHECK(res == 8);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
