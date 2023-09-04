c8785f44cc37cb54e164f776fcbaecf6e5f7cfe7
Marcin Ickiewicz <mickiewicz@syncad.com>
2023-09-04 10:09:32 +0200

    psql_utils: decrease log severity for RAII exceptions

--- common_includes/include/exceptions.hpp
+++ common_includes/include/exceptions.hpp
@@ -9,8 +9,17 @@
 LOG_WARNING( "Throw exception: %s",  std::string( _message ).c_str() );          \
 throw std::runtime_error( _message )                                             \
 
+/**
+ * THROW_INITIALIZATION_ERROR macro to implement RAII pattern
+ *
+ * This macro throws an ObjectInitializationException when an object
+ * can't be created, usually due to resource access issues. While this
+ * is a serious problem for the object, it might not be as critical
+ * for the client code. The macro logs the exception with DEBUG severity
+ * to help with debugging.
+ */
 #define THROW_INITIALIZATION_ERROR( _message )                                        \
-LOG_WARNING( "Throw inialization exception: %s", std::string( _message ).c_str() );   \
+LOG_DEBUG( "Throw initialization exception: %s", std::string( _message ).c_str() );   \
 throw ObjectInitializationException( _message )                                       \
 
 
