e064674ab5f463067946dbdb342d8f85ee956427
Eric Frias <efrias@syncad.com>
2024-01-02 23:23:39 +0000

    Add uninstall_app script to setup docker image

--- docker/setup_entrypoint.sh
+++ docker/setup_entrypoint.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 set -e
 cd /hafah/scripts
-./setup_postgres.sh --postgres-url=${POSTGRES_URL}
-./install_app.sh --postgres-url=${POSTGRES_URL}
+
+if [ "$1" = "install_app" ]; then
+  ./setup_postgres.sh --postgres-url=${POSTGRES_URL}
+  ./install_app.sh --postgres-url=${POSTGRES_URL}
+elif [ "$1" = "uninstall_app" ]; then
+  ./uninstall_app.sh --postgres-url=${POSTGRES_URL}
+else
+  echo "usage: $0 install_app|uninstall_app"
+  exit 1
+fi
+
