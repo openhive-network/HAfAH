33d76002117eaa06de160d9a94c0a03254286751
Radek Masłowski <rmaslowski@syncad.com>
2024-01-10 15:06:11 +0100

    Repair send_request_to_hafah function

--- tests/integration/hafah-local-tools/hafah_local_tools/__init__.py
+++ tests/integration/hafah-local-tools/hafah_local_tools/__init__.py
@@ -1,10 +1,19 @@
 import test_tools as tt
 import time
 
+from helpy.exceptions import RequestError
+
+
 def send_request_to_hafah(hafah_node: tt.RemoteNode, method, **kwargs):
     previous_response = None
-    for _ in range(5):
-        response = getattr(hafah_node.api.account_history, method)(**kwargs)
+    for i in range(5):
+        try:
+            response = getattr(hafah_node.api.account_history, method)(**kwargs)
+        except RequestError as error:
+            if "Unknown Transaction" in error.error and i!=4:
+                response = None
+            else:
+                raise
         if previous_response != response and previous_response is not None:
             return response
         previous_response = response
