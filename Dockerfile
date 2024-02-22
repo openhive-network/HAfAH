7fb3a80a337d1df9cf95c381efe9dd95a7de2a27
Konrad Botor <kbotor@syncad.com>
2024-02-22 10:14:23 +0100

    Added Docker labels

--- Dockerfile
+++ Dockerfile
@@ -23,6 +23,27 @@ SHELL ["/bin/bash", "-c"]
 
 FROM runtime AS instance
 
+ARG BUILD_TIME
+ARG GIT_COMMIT_SHA
+ARG GIT_CURRENT_BRANCH
+ARG GIT_LAST_LOG_MESSAGE
+ARG GIT_LAST_COMMITTER
+ARG GIT_LAST_COMMIT_DATE
+LABEL org.opencontainers.image.created="$BUILD_TIME"
+LABEL org.opencontainers.image.url="https://hive.io/"
+LABEL org.opencontainers.image.documentation="https://gitlab.syncad.com/hive/HAfAH"
+LABEL org.opencontainers.image.source="https://gitlab.syncad.com/hive/HAfAH"
+#LABEL org.opencontainers.image.version="${VERSION}"
+LABEL org.opencontainers.image.revision="$GIT_COMMIT_SHA"
+LABEL org.opencontainers.image.licenses="MIT"
+LABEL org.opencontainers.image.ref.name="HAfAH"
+LABEL org.opencontainers.image.title="HAF Account History (HAfAH) Image"
+LABEL org.opencontainers.image.description="Runs HAfAH application"
+LABEL io.hive.image.branch="$GIT_CURRENT_BRANCH"
+LABEL io.hive.image.commit.log_message="$GIT_LAST_LOG_MESSAGE"
+LABEL io.hive.image.commit.author="$GIT_LAST_COMMITTER"
+LABEL io.hive.image.commit.date="$GIT_LAST_COMMIT_DATE"
+
 ARG HTTP_PORT=6543
 ENV HTTP_PORT=${HTTP_PORT}
 
--- scripts/ci-helpers/build_instance.sh
+++ scripts/ci-helpers/build_instance.sh
@@ -74,11 +74,46 @@ pushd "$SOURCE_DIR"
 
 bash "./scripts/generate_version_sql.bash" "$(pwd)"
 
-export DOCKER_BUILDKIT=1
+BUILD_TIME="$(date -uIseconds)"
 
-docker build \
+GIT_COMMIT_SHA="$(git rev-parse HEAD || true)"
+if [ -z "$GIT_COMMIT_SHA" ]; then
+  GIT_COMMIT_SHA="[unknown]"
+fi
+
+GIT_CURRENT_BRANCH="$(git branch --show-current || true)"
+if [ -z "$GIT_CURRENT_BRANCH" ]; then
+  GIT_CURRENT_BRANCH="$(git describe --abbrev=0 --all | sed 's/^.*\///' || true)"
+  if [ -z "$GIT_CURRENT_BRANCH" ]; then
+    GIT_CURRENT_BRANCH="[unknown]"
+  fi
+fi
+
+
+GIT_LAST_LOG_MESSAGE="$(git log -1 --pretty=%B || true)"
+if [ -z "$GIT_LAST_LOG_MESSAGE" ]; then
+  GIT_LAST_LOG_MESSAGE="[unknown]"
+fi
+
+GIT_LAST_COMMITTER="$(git log -1 --pretty="%an <%ae>" || true)"
+if [ -z "$GIT_LAST_COMMITTER" ]; then
+  GIT_LAST_COMMITTER="[unknown]"
+fi
+
+GIT_LAST_COMMIT_DATE="$(git log -1 --pretty="%aI" || true)"
+if [ -z "$GIT_LAST_COMMIT_DATE" ]; then
+  GIT_LAST_COMMIT_DATE="[unknown]"
+fi
+
+docker buildx build \
     --build-arg HTTP_PORT="$APP_PORT" \
     --build-arg POSTGRES_URL="$HAF_POSTGRES_URL" \
+    --build-arg BUILD_TIME="$BUILD_TIME" \
+    --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" \
+    --build-arg GIT_CURRENT_BRANCH="$GIT_CURRENT_BRANCH" \
+    --build-arg GIT_LAST_LOG_MESSAGE="$GIT_LAST_LOG_MESSAGE" \
+    --build-arg GIT_LAST_COMMITTER="$GIT_LAST_COMMITTER" \
+    --build-arg GIT_LAST_COMMIT_DATE="$GIT_LAST_COMMIT_DATE" \
     --target=instance \
     --tag "$HAFAH_IMAGE_NAME" \
     --file Dockerfile .
