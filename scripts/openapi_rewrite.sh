f1b596053282da2732b8c1a892539c10489298f2
Michal Zander <mzander@syncad.com>
2024-07-24 11:21:27 +0200

    Update openapi rewrite script

--- scripts/openapi_rewrite.sh
+++ scripts/openapi_rewrite.sh
@@ -4,46 +4,26 @@ set -e
 set -o pipefail
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1; pwd -P )"
-endpoints_dir="../postgrest/hafah_REST"
+
+haf_dir="../haf"
+endpoints="postgrest/hafah_REST"
+rewrite_dir="${endpoints}_openapi"
 input_file="rewrite_rules.conf"
 temp_output_file=$(mktemp)
 
-
-cat <<EOF
-  Script used to search for all SQL scripts with openapi descriptions...
-
-  Usage: $0 <output_directory> <type_directories> <endpoint_directories>
-  
-EOF
-
 # Default directories with fixed order if none provided
-DEFAULT_OUTPUT="$SCRIPTDIR/output"
-
-DEFAULT_ENDPOINTS="
-$endpoints_dir/hafah_openapi.sql
-
-$endpoints_dir/blocks/get_block_range.sql
-$endpoints_dir/blocks/get_block.sql
-$endpoints_dir/blocks/get_block_header.sql
-$endpoints_dir/blocks/get_ops_in_block.sql
-
-$endpoints_dir/operations/get_operations.sql
-$endpoints_dir/operations/enum_virtual_ops.sql
-
-$endpoints_dir/transactions/get_transaction.sql
-
-$endpoints_dir/accounts/get_account_history.sql
-
-$endpoints_dir/other/get_version.sql
-
-"
-
-echo "Using default HAF block explorer types and endpoints directories"
-
-echo "$DEFAULT_ENDPOINTS"
-
-# shellcheck disable=SC2086
-python3 ../haf/scripts/process_openapi.py $DEFAULT_OUTPUT $DEFAULT_ENDPOINTS
+OUTPUT="$SCRIPTDIR/output"
+ENDPOINTS_IN_ORDER="
+../$endpoints/hafah_openapi.sql
+../$endpoints/blocks/get_block_range.sql
+../$endpoints/blocks/get_block.sql
+../$endpoints/blocks/get_block_header.sql
+../$endpoints/blocks/get_ops_in_block.sql
+../$endpoints/operations/get_operations.sql
+../$endpoints/operations/enum_virtual_ops.sql
+../$endpoints/transactions/get_transaction.sql
+../$endpoints/accounts/get_account_history.sql
+../$endpoints/other/get_version.sql"
 
 # Function to reverse the lines
 reverse_lines() {
@@ -68,6 +48,60 @@ reverse_lines() {
     }' "$input_file" | tac
 }
 
+# Function to install pip3
+install_pip() {
+    echo "pip3 is not installed. Installing now..."
+    # Ensure Python 3 is installed
+    if ! command -v python3 &> /dev/null; then
+        echo "Python 3 is not installed. Please install Python 3 first."
+        exit 1
+    fi
+    # Try to install pip3
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
+    if ! command -v pip3 &> /dev/null; then
+        echo "pip3 installation failed. Please install pip3 manually."
+        exit 1
+    fi
+}
+
+# Check if pip3 is installed
+if ! command -v pip3 &> /dev/null; then
+    install_pip
+fi
+
+# Check if deepmerge is installed
+if python3 -c "import deepmerge" &> /dev/null; then
+    echo "deepmerge is already installed."
+else
+    echo "deepmerge is not installed. Installing now..."
+    pip3 install deepmerge
+    echo "deepmerge has been installed."
+fi
+
+# Check if jsonpointer is installed
+if python3 -c "import jsonpointer" &> /dev/null; then
+    echo "jsonpointer is already installed."
+else
+    echo "jsonpointer is not installed. Installing now..."
+    pip3 install jsonpointer
+    echo "jsonpointer has been installed."
+fi
+
+echo "Using endpoints directories"
+echo "$ENDPOINTS_IN_ORDER"
+
+# run openapi rewrite script
+# shellcheck disable=SC2086
+python3 $haf_dir/scripts/process_openapi.py $OUTPUT $ENDPOINTS_IN_ORDER
+
+# Create rewrite_rules.conf
 reverse_lines > "$temp_output_file"
-mv "$temp_output_file" "$input_file"
-mv "$input_file" "../$input_file"
+mv "$temp_output_file" "../$input_file"
+rm "$input_file"
+
+# Move rewriten directory to /postgrest
+rm -rf "$SCRIPTDIR/../$rewrite_dir"
+mv "$OUTPUT/../$endpoints" "$SCRIPTDIR/../$rewrite_dir"
+rm -rf $SCRIPTDIR/output 
+echo "Rewritten scripts saved in $rewrite_dir"
