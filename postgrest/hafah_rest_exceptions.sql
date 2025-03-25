7dc7695e787479d6683d2418e54ce8d2c2cd9757
Michal Zander <mzander@syncad.com>
2025-03-25 11:35:11 +0000

    Adjust exception messages

--- postgrest/hafah_rest_exceptions.sql
+++ postgrest/hafah_rest_exceptions.sql
@@ -1,85 +1,85 @@
 SET ROLE hafah_owner;
 
-CREATE FUNCTION hafah_backend.rest_raise_missing_account(_account_name TEXT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_missing_account(_account_name TEXT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:Account ''%s'' does not exist', _account_name;
+  RAISE EXCEPTION 'Account ''%'' does not exist', _account_name;
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.rest_raise_missing_block(_block_num INT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_missing_block(_block_num INT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:block_num ''%s'' does not exist', _block_num;
+  RAISE EXCEPTION 'Block_num ''%'' does not exist', _block_num;
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.rest_raise_missing_arg(_account_name TEXT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_missing_arg(_account_name TEXT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:Missing a required argument: ''%s''', _arg_name;
+  RAISE EXCEPTION 'Missing a required argument: ''%''', _arg_name;
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.raise_uint_exception()
+CREATE OR REPLACE FUNCTION hafah_backend.raise_uint_exception()
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:Couldn''t parse uint64_t';
+  RAISE EXCEPTION 'Couldn''t parse uint64_t';
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.rest_raise_account_name_too_long(_account_name TEXT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_account_name_too_long(_account_name TEXT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:in_len <= sizeof(data): Input too large: `%s` (%s) for fixed size string: (16)', _account_name, LENGTH(_account_name);
+  RAISE EXCEPTION 'in_len <= sizeof(data): Input too large: `%` (%) for fixed size string: (16)', _account_name, LENGTH(_account_name);
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.rest_raise_invalid_char_in_hex(_hex TEXT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_invalid_char_in_hex(_hex TEXT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:Invalid hex character ''%s''', left(ltrim(_hex, '0123456789abcdefABCDEF'), 1);
+  RAISE EXCEPTION 'Invalid hex character ''%''', left(ltrim(_hex, '0123456789abcdefABCDEF'), 1);
 END
 $$
 ;
 
-CREATE FUNCTION hafah_backend.rest_raise_transaction_hash_invalid_length(_hex TEXT)
+CREATE OR REPLACE FUNCTION hafah_backend.rest_raise_transaction_hash_invalid_length(_hex TEXT)
 RETURNS VOID
 LANGUAGE 'plpgsql'
 IMMUTABLE
 AS
 $$
 BEGIN
-  RAISE EXCEPTION 'Assert Exception:false: Transaction hash ''%s'' has invalid size. Transaction hash should have size of 160 bits', _hex;
+  RAISE EXCEPTION 'false: Transaction hash ''%'' has invalid size. Transaction hash should have size of 160 bits', _hex;
 END
 $$
 ;
