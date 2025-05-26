b9628d0370e4a8335f8e4dd1e01953832ed22bfb
Michal Zander <mzander@syncad.com>
2025-05-26 14:37:06 +0000

    Add fill_order type

--- scripts/openapi_rewrite.sh
+++ scripts/openapi_rewrite.sh
@@ -19,6 +19,7 @@ ENDPOINTS_IN_ORDER="
 ../$endpoints/types/sort_direction.sql
 ../$endpoints/types/block.sql
 ../$endpoints/types/transaction.sql
+../$endpoints/types/fill_order.sql
 ../$endpoints/hafah_openapi.sql
 ../$endpoints/blocks/get_block_range.sql
 ../$endpoints/blocks/get_block.sql
