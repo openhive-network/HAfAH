2346b1fc4c6873aea4315f0f2fadc28f849acaf6
Michal Zander <mzander@syncad.com>
2025-07-18 11:04:24 +0000

    Additional paging logic for corner case when filtering by transacting_account_id

--- queries/hafah_rest_backend/account_history/filtering_functions/including_accounts.sql
+++ queries/hafah_rest_backend/account_history/filtering_functions/including_accounts.sql
@@ -35,12 +35,8 @@ BEGIN
     SELECT
       ls.operation_id AS id,
       ls.block_num,
-      ov.trx_in_block,
-      encode(htv.trx_hash, 'hex') AS trx_hash,
-      ov.op_pos,
       ls.op_type_id,
-      ov.body,
-      hot.is_virtual
+      ROW_NUMBER() OVER (ORDER BY ls.operation_id DESC) AS row_num -- used to determine if last 2 records are in the same block (when page is saturated)
     FROM (
       SELECT aov.operation_id, aov.op_type_id, aov.block_num
       FROM hive.account_operations_view aov
@@ -50,25 +46,94 @@ BEGIN
       AND aov.account_op_seq_no >= _account_range.from_seq
       AND aov.account_op_seq_no <= _account_range.to_seq
       ORDER BY aov.account_op_seq_no DESC
-      LIMIT (__max_page_count * _limit) -- by default operation filter is limited to 10 pages
+      LIMIT (__max_page_count * _limit) + 1 -- by default operation filter is limited to 10 pages
+      -- The +1 is to check if there are more operations in block that are not included in the current page-range
     ) ls
-    JOIN hive.operations_view ov ON ov.id = ls.operation_id
-    JOIN hafd.operation_types hot ON hot.id = ls.op_type_id
-    LEFT JOIN hive.transactions_view htv ON htv.block_num = ls.block_num AND htv.trx_in_block = ov.trx_in_block
   ),
   -----------PAGING LOGIC----------------
-  -- Pages are counted differently compared to default.sql
-  -- The results are based on a set of blocks for each operation, 
-  -- so the count and total number of pages depend on the specific query inside this cte chain
+  -- Calculating pages based on result set (operation_range) 
+  -- there is corner case when the last two operations are in the same block
+  -- when generating next batch of pages, we may miss operations made in the same block
+  -- to prevent this, we check if the last two operations are in the same block
+  -- if there at least 2 operations in the same block, we fetch all operations in that block
+  -- and add them to the result set
+  -- and calculate pages based on the new result set
+  check_if_saturated AS ( -- if not saturated, returns empty
+    SELECT (
+      CASE
+        WHEN MAX(row_num) = (__max_page_count * _limit) + 1 THEN 
+          (__max_page_count * _limit) + 1
+        ELSE 
+          NULL
+      END 
+    ) AS count
+    FROM operation_range
+  ), 
+  if_saturated_find_last_two_ops AS ( -- if not saturated, returns empty
+    SELECT
+      orr.block_num
+    FROM operation_range orr
+    WHERE orr.row_num IN (
+      (SELECT count FROM check_if_saturated), 
+      (SELECT count - 1 FROM check_if_saturated)
+    )
+  ), 
+  block_check AS MATERIALIZED ( -- if not saturated, returns empty
+    SELECT (
+      CASE
+        WHEN COUNT(DISTINCT block_num) = 1 THEN 
+          MIN(block_num)
+        ELSE 
+          NULL
+      END  
+    ) AS block_num
+    FROM if_saturated_find_last_two_ops
+  ),
+  -- returns empty if the last two operations are not in the same block
+  -- if the last two operations are in the same block, next CTE returns all operations in that block
+  find_all_records_for_page AS ( -- if not saturated, returns empty
+    SELECT 
+      aov.operation_id AS id, 
+      aov.op_type_id,
+      aov.block_num
+    FROM hive.account_operations_view aov
+    WHERE aov.account_id = _account_id
+    AND aov.transacting_account_id = _transacting_account_id
+    AND (_operations IS NULL OR aov.op_type_id = ANY(_operations))
+    AND aov.account_op_seq_no >= _account_range.from_seq
+    AND aov.account_op_seq_no <= _account_range.to_seq
+    AND (
+	    (SELECT block_num FROM block_check) IS NOT NULL 
+	    AND aov.block_num = (SELECT block_num FROM block_check)
+	  ) 
+  ),
+  union_operations AS MATERIALIZED (
+    SELECT
+      id,
+      block_num,
+      op_type_id
+    FROM operation_range
+    WHERE row_num <= (__max_page_count * _limit) -- limit to the maximum number of rows for the page and remove the extra row
+    AND ((SELECT block_num FROM block_check) IS NULL OR block_num != (SELECT block_num FROM block_check) ) 
+    -- if block_check is not NULL, exclude the operations from last block
+    -- operations from excluded block are fetched in find_all_records_for_page
+    UNION ALL
+
+    SELECT
+      id,
+      block_num,
+      op_type_id
+    FROM find_all_records_for_page
+  ),
   min_block_num AS (
     SELECT 
       MIN(block_num) AS block_num
-    FROM operation_range
+    FROM union_operations
   ),
   count_blocks AS MATERIALIZED (
     SELECT 
       COUNT(*) AS count
-    FROM operation_range
+    FROM union_operations
   ),
   calculate_pages AS MATERIALIZED (
     SELECT 
@@ -82,12 +147,32 @@ BEGIN
       _limit
     )
   ),
-  filter_page AS MATERIALIZED (
+  filter_page AS (
     SELECT *
-    FROM operation_range
+    FROM union_operations
     ORDER BY id DESC
     OFFSET (SELECT offset_filter FROM calculate_pages)
     LIMIT (SELECT limit_filter FROM calculate_pages)
+  ),
+  -----------END PAGING LOGIC----------------
+  -- join the operations with other necessary tables 
+  join_tables AS (
+    SELECT
+      ls.id,
+      ls.block_num,
+      ov.trx_in_block,
+      encode(htv.trx_hash, 'hex') AS trx_hash,
+      ov.op_pos,
+      ls.op_type_id,
+      ov.body,
+      hot.is_virtual
+    FROM (
+      SELECT aov.id, aov.op_type_id, aov.block_num
+      FROM filter_page aov
+    ) ls
+    JOIN hive.operations_view ov ON ov.id = ls.id
+    JOIN hafd.operation_types hot ON hot.id = ls.op_type_id
+    LEFT JOIN hive.transactions_view htv ON htv.block_num = ls.block_num AND htv.trx_in_block = ov.trx_in_block
   ),
   -- filter too long operation bodies 
   result_query AS (
@@ -103,7 +188,7 @@ BEGIN
       filtered_operations.trx_in_block
     FROM (
       SELECT hafah_backend.operation_body_filter(ov.body, ov.id, _body_limit) as composite, ov.id, ov.block_num, ov.trx_in_block, ov.trx_hash, ov.op_pos, ov.op_type_id, ov.is_virtual, hb.created_at
-      FROM filter_page ov 
+      FROM join_tables ov 
       JOIN hive.blocks_view hb ON hb.num = ov.block_num
     ) filtered_operations
     ORDER BY filtered_operations.id DESC
@@ -138,7 +223,7 @@ BEGIN
     CASE
       WHEN __min_block_num IS NULL THEN _account_range.from_block
       WHEN __min_block_num IS NOT NULL AND __min_block_num = 1 THEN 1
-      WHEN __min_block_num IS NOT NULL AND __min_block_num != 1 AND __count != __max_page_count * _limit THEN _account_range.from_block
+      WHEN __min_block_num IS NOT NULL AND __min_block_num != 1 AND __count < __max_page_count * _limit THEN _account_range.from_block
       ELSE __min_block_num - 1
     END
   );
